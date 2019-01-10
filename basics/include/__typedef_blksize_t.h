a4e0a3a61b4de9a28d0adda01b4fb593ed962826
Dan Gohman <sunfish@mozilla.com>
2019-01-10 11:26:59 -0800

    Reconcile some typedefs with Musl's versions.

--- basics/include/__typedef_blksize_t.h
+++ basics/include/__typedef_blksize_t.h
@@ -1,7 +1,6 @@
 #ifndef __wasm_basics___typedef_blksize_t_h
 #define __wasm_basics___typedef_blksize_t_h
 
-/* Define these as 64-bit signed integers to support files larger than 2 GiB. */
-typedef long long blksize_t;
+typedef long blksize_t;
 
 #endif

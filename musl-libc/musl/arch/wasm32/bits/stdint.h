04084b6109ac0d6f0bf4a483df96138bf75b17ed
Dan Gohman <sunfish@mozilla.com>
2019-01-10 11:03:33 -0800

    Change int_fast16_t/uint_fast16_t to 16-bit.
    Clang's stdint.h doesn't support making them 32-bit. See
    https://reviews.llvm.org/D41941 for details.

--- musl-libc/musl/arch/wasm32/bits/stdint.h
+++ musl-libc/musl/arch/wasm32/bits/stdint.h
@@ -1,15 +1,15 @@
-typedef int32_t int_fast16_t;
+typedef int16_t int_fast16_t;
 typedef int32_t int_fast32_t;
-typedef uint32_t uint_fast16_t;
+typedef uint16_t uint_fast16_t;
 typedef uint32_t uint_fast32_t;
 
-#define INT_FAST16_MIN  INT32_MIN
+#define INT_FAST16_MIN  INT16_MIN
 #define INT_FAST32_MIN  INT32_MIN
 
-#define INT_FAST16_MAX  INT32_MAX
+#define INT_FAST16_MAX  INT16_MAX
 #define INT_FAST32_MAX  INT32_MAX
 
-#define UINT_FAST16_MAX UINT32_MAX
+#define UINT_FAST16_MAX UINT16_MAX
 #define UINT_FAST32_MAX UINT32_MAX
 
 #define INTPTR_MIN      INT32_MIN

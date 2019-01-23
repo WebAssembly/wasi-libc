70c36eaaa1e144e04ebc25c9be671034cb11e411
Dan Gohman <sunfish@mozilla.com>
2019-01-22 20:40:06 -0800

    Switch the triple to wasm32-unknown-wasmi.
    This removes the -musl from the triple, since we don't have a
    conventional musl port.

--- libc-top-half/musl/include/setjmp.h
+++ libc-top-half/musl/include/setjmp.h
@@ -35,7 +35,7 @@ _Noreturn void longjmp (jmp_buf, int);
 
 #define setjmp setjmp
 #else
-#warning setjmp is not yet implemented for wasm32-unknown-wasi-musl
+#warning setjmp is not yet implemented for wasm32-unknown-wasi
 #endif
 
 #ifdef __cplusplus

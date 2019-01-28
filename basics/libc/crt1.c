8b482ef0a6244c06f2ae52f0b29768da24c65456
Dan Gohman <sunfish@mozilla.com>
2019-01-28 05:29:31 -0800

    Call musl's exit cleanups even when not calling _Exit.
    Also, call __wasm_call_ctors.

--- basics/libc/crt1.c
+++ basics/libc/crt1.c
@@ -1,10 +1,17 @@
 extern int main(int, char *[]);
-void exit(int) __attribute__((__noreturn__));
+void _Exit(int) __attribute__((__noreturn__));
+extern void __prepare_for_exit(void);
+extern void __wasm_call_ctors(void);
 
 void _start(void) {
+    __wasm_call_ctors();
+
     /* For now, just call main with no arguments. */
     int r = main(0, 0);
+
+    __prepare_for_exit();
+
     if (r != 0) {
-        exit(r);
+        _Exit(r);
     }
 }
--- libc-top-half/musl/src/exit/exit.c
+++ libc-top-half/musl/src/exit/exit.c
@@ -12,6 +12,7 @@ weak_alias(dummy, __funcs_on_exit);
 weak_alias(dummy, __stdio_exit);
 weak_alias(dummy, _fini);
 
+#ifdef __wasilibc_unmodified_upstream__ // fini
 extern weak hidden void (*const __fini_array_start)(void), (*const __fini_array_end)(void);
 
 static void libc_exit_fini(void)
@@ -23,7 +24,9 @@ static void libc_exit_fini(void)
 }
 
 weak_alias(libc_exit_fini, __libc_exit_fini);
+#endif
 
+#ifdef __wasilibc_unmodified_upstream__
 _Noreturn void exit(int code)
 {
 	__funcs_on_exit();
@@ -31,3 +34,19 @@ _Noreturn void exit(int code)
 	__stdio_exit();
 	_Exit(code);
 }
+#else
+// Split out the cleanup functions so that we can call them without calling
+// _Exit if we don't need to. This allows _start to just return if main
+// returns 0.
+void __prepare_for_exit(void)
+{
+	__funcs_on_exit();
+	__stdio_exit();
+}
+
+_Noreturn void exit(int code)
+{
+	__prepare_for_exit();
+	_Exit(code);
+}
+#endif

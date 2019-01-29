4d40cb3cbac912830d6f1450d9074bbe5de18614
Dan Gohman <sunfish@mozilla.com>
2019-01-29 07:32:49 -0800

    Move libc-bottom-half's netinet/in.h header contents into a sharable header.

--- libc-bottom-half/headers/public/netinet/in.h
+++ libc-bottom-half/headers/public/netinet/in.h
@@ -1,14 +1,6 @@
 #ifndef __wasilibc_netinet_in_h
 #define __wasilibc_netinet_in_h
 
-#include <__struct_sockaddr_in.h>
-#include <__struct_sockaddr_in6.h>
-
-#define IPPROTO_IP 0
-#define IPPROTO_ICMP 1
-#define IPPROTO_TCP 6
-#define IPPROTO_UDP 17
-#define IPPROTO_IPV6 41
-#define IPPROTO_RAW 255
+#include <__header_netinet_in.h>
 
 #endif

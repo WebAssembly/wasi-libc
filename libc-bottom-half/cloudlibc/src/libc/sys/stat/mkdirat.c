f263fea34c993c96c2bfe0871772623e35a3f2cb
Dan Gohman <sunfish@mozilla.com>
2019-02-14 16:27:18 -0800

    Remove `file_create` and add `file_mkdir`.
    Besides being a slight simplification (the only current use of
    `file_create` is creating directories), if we add ways to create
    other kinds of filesystem objects in the future, it's desirable
    to create separate functions for them.
    
    WASI has two levels of capabilities -- static and dynamic. The
    static capabilities are the imports. By using separate functions
    for separate file types, we can distinguish between the
    capabilities to create different types of filesystem objects
    statically.
    
    And, users won't often need to specify what type of thing to create
    dynamicaly. For example, the POSIX interface just has `mkdir`.

--- libc-bottom-half/cloudlibc/src/libc/sys/stat/mkdirat.c
+++ libc-bottom-half/cloudlibc/src/libc/sys/stat/mkdirat.c
@@ -15,8 +15,13 @@ int mkdirat(int fd, const char *path, ...) {
 #else
 int mkdirat(int fd, const char *path, mode_t mode) {
 #endif
+#ifdef __wasilibc_unmodified_upstream // __wasi_file_mkdir
   __wasi_errno_t error = __wasi_file_create(
       fd, path, strlen(path), __WASI_FILETYPE_DIRECTORY);
+#else
+  __wasi_errno_t error = __wasi_file_mkdir(
+      fd, path, strlen(path));
+#endif
   if (error != 0) {
     errno = errno_fixup_directory(fd, error);
     return -1;

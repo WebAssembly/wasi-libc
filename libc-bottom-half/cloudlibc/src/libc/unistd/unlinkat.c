6e83f0c71f0a82da79a4bec12ca416ce58be386a
Dan Gohman <sunfish@mozilla.com>
2019-03-01 11:46:17 -0800

    Split file_unlink into file and directory forms.
    Split file_unlink into file_unlink_file and file_unlink_directory.
    Rather than one function with a flags argument to say which of two
    things to do, having two functions allows finer-grained import
    capabilities.
    
    Also, standardize the errno choices for the unlink functions, in
    cases where underlying Unix platforms differ.

--- libc-bottom-half/cloudlibc/src/libc/unistd/unlinkat.c
+++ libc-bottom-half/cloudlibc/src/libc/unistd/unlinkat.c
@@ -11,6 +11,7 @@
 #include <unistd.h>
 
 int unlinkat(int fd, const char *path, int flag) {
+#ifdef __wasilibc_unmodified_upstream // unlink
   __wasi_ulflags_t ulflags = 0;
   if ((flag & AT_REMOVEDIR) != 0)
     ulflags |= __WASI_UNLINK_REMOVEDIR;
@@ -20,5 +21,22 @@ int unlinkat(int fd, const char *path, int flag) {
     errno = errno_fixup_directory(fd, error);
     return -1;
   }
+#else
+  __wasi_errno_t error;
+  size_t path_len = strlen(path);
+  if ((flag & AT_REMOVEDIR) != 0) {
+    error = __wasi_file_unlink_directory(fd, path, path_len);
+    if (error != 0) {
+      errno = errno_fixup_directory(fd, error);
+      return -1;
+    }
+  } else {
+    error = __wasi_file_unlink_file(fd, path, path_len);
+    if (error != 0) {
+      errno = error;
+      return -1;
+    }
+  }
+#endif
   return 0;
 }

c834e3b5f87a8d0ed2dde416a58446436a833da8
Dan Gohman <sunfish@mozilla.com>
2019-03-01 11:21:50 -0800

    Add function attributes to memcpy, malloc, and friends.

--- libc-bottom-half/headers/public/stdlib.h
+++ libc-bottom-half/headers/public/stdlib.h
@@ -1,11 +1,10 @@
 #ifndef __wasilibc_stdlib_h
 #define __wasilibc_stdlib_h
 
-#define __need_size_t
-#include <stddef.h>
-
-void abort(void) __attribute__ ((__noreturn__));
-void qsort(void *, size_t, size_t,
-           int (*)(const void *, const void *));
+/*
+ * Include the real implementation, which is factored into a separate file so
+ * that it can be reused by other libc stdlib implementations.
+ */
+#include <__headers_stdlib.h>
 
 #endif

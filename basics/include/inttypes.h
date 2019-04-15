ad22a590135b1ea3caa747fa7b6b1ccba616a552
Dan Gohman <sunfish@mozilla.com>
2019-04-15 05:00:57 -0700

    Add a basics implementation of <inttypes.h>.
    Musl's generic definitions for PRIxPTR and several others in
    <inttypes.h> are incorrect for wasm, so introduce a new <inttypes.h>
    and <__header_inttypes.h> in the basics module using compiler-provided
    definitions.
    
    Fixes #13.

---
+++ basics/include/inttypes.h
@@ -0,0 +1,10 @@
+#ifndef __wasm_basics_inttypes_h
+#define __wasm_basics_inttypes_h
+
+/*
+ * Include the real implementation, which is factored into a separate file so
+ * that it can be reused by other libc stdlib implementations.
+ */
+#include <__header_inttypes.h>
+
+#endif

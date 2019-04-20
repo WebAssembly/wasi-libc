49f1a57cb8b16bd108bb6e2206c2b98e6007e18a
Dan Gohman <sunfish@mozilla.com>
2019-04-19 17:19:55 -0700

    Declare getrusage.
    It's defined publicly in libc, so publicly declare it too.

--- libc-bottom-half/headers/public/__header_sys_resource.h
+++ libc-bottom-half/headers/public/__header_sys_resource.h
@@ -6,4 +6,14 @@
 #define RUSAGE_SELF 1
 #define RUSAGE_CHILDREN 2
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int getrusage(int who, struct rusage *usage);
+
+#ifdef __cplusplus
+}
+#endif
+
 #endif

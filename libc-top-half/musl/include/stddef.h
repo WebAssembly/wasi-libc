33e8b5189bbf02f3a18d933e9b79e7d134f2d76c
Dan Gohman <sunfish@mozilla.com>
2019-04-29 05:52:26 -0700

    Omit musl's stdarg.h and stddef.h from the sysroot.
    We already use the compiler's versions of these; this just moves from
    having libc #include_next them to having libc just omit them entirely,
    which is simpler.
    
    This removes the special code to define musl's include guard, however
    I originally added that when I was still working out how WASI's stddef.h
    would interact with other code. I believe it's no longer important.

--- libc-top-half/musl/include/stddef.h
+++ libc-top-half/musl/include/stddef.h
@@ -1,4 +1,3 @@
-#ifdef __wasilibc_unmodified_upstream /* Use the compiler's stddef.h */
 #ifndef _STDDEF_H
 #define _STDDEF_H
 
@@ -21,17 +20,6 @@
 #define offsetof(type, member) __builtin_offsetof(type, member)
 #else
 #define offsetof(type, member) ((size_t)( (char *)&(((type *)0)->member) - (char *)0 ))
-#endif
-
-#endif
-#else
-
-/* Just use the compiler's stddef.h. */
-#include_next <stddef.h>
-
-/* Define musl's include guard, in case any code depends on that. */
-#if defined(__STDDEF_H) && !defined(_STDDEF_H)
-#define _STDDEF_H
 #endif
 
 #endif

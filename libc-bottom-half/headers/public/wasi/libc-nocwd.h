59b808d5ca90da69767baf89b40805ec80df8e46
Dan Gohman <sunfish@mozilla.com>
2019-05-07 20:57:47 -0700

    Make libc-nocwd.h more robust.
    Include the headers for the types that it uses, and add
    warn_unused_result annotations to its functions.

--- libc-bottom-half/headers/public/wasi/libc-nocwd.h
+++ libc-bottom-half/headers/public/wasi/libc-nocwd.h
@@ -8,26 +8,49 @@
  * assume AT_FDCWD has already been resolved.
  */
 
+#define __need_size_t
+#include <stddef.h>
+#include <__typedef_ssize_t.h>
+#include <__typedef_mode_t.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-int __wasilibc_nocwd___wasilibc_unlinkat(int, const char *);
-int __wasilibc_nocwd___wasilibc_rmdirat(int, const char *);
-int __wasilibc_nocwd_linkat(int, const char *, int, const char *, int);
-int __wasilibc_nocwd_symlinkat(const char *, int, const char *);
-ssize_t __wasilibc_nocwd_readlinkat(int, const char *__restrict, char *__restrict, size_t);
-int __wasilibc_nocwd_unlinkat(int, const char *, int);
-int __wasilibc_nocwd_faccessat(int, const char *, int, int);
-int __wasilibc_nocwd_renameat(int, const char *, int, const char *);
-int __wasilibc_nocwd_openat(int, const char *, int, ...);
-int __wasilibc_nocwd_fstatat(int, const char *__restrict, struct stat *__restrict, int);
-int __wasilibc_nocwd_mkdirat(int, const char *, mode_t);
-int __wasilibc_nocwd_utimensat(int, const char *, const struct timespec [2], int);
-DIR *__wasilibc_nocwd_opendirat(int, const char *);
+struct timespec;
+struct stat;
+struct dirent;
+
+int __wasilibc_nocwd___wasilibc_unlinkat(int, const char *)
+    __attribute__((__warn_unused_result__));
+int __wasilibc_nocwd___wasilibc_rmdirat(int, const char *)
+    __attribute__((__warn_unused_result__));
+int __wasilibc_nocwd_linkat(int, const char *, int, const char *, int)
+    __attribute__((__warn_unused_result__));
+int __wasilibc_nocwd_symlinkat(const char *, int, const char *)
+    __attribute__((__warn_unused_result__));
+ssize_t __wasilibc_nocwd_readlinkat(int, const char *__restrict, char *__restrict, size_t)
+    __attribute__((__warn_unused_result__));
+int __wasilibc_nocwd_unlinkat(int, const char *, int)
+    __attribute__((__warn_unused_result__));
+int __wasilibc_nocwd_faccessat(int, const char *, int, int)
+    __attribute__((__warn_unused_result__));
+int __wasilibc_nocwd_renameat(int, const char *, int, const char *)
+    __attribute__((__warn_unused_result__));
+int __wasilibc_nocwd_openat(int, const char *, int, ...)
+    __attribute__((__warn_unused_result__));
+int __wasilibc_nocwd_fstatat(int, const char *__restrict, struct stat *__restrict, int)
+    __attribute__((__warn_unused_result__));
+int __wasilibc_nocwd_mkdirat(int, const char *, mode_t)
+    __attribute__((__warn_unused_result__));
+int __wasilibc_nocwd_utimensat(int, const char *, const struct timespec [2], int)
+    __attribute__((__warn_unused_result__));
+DIR *__wasilibc_nocwd_opendirat(int, const char *)
+    __attribute__((__warn_unused_result__));
 int __wasilibc_nocwd_scandirat(int, const char *, struct dirent ***,
                                int (*)(const struct dirent *),
-                               int (*)(const struct dirent **, const struct dirent **));
+                               int (*)(const struct dirent **, const struct dirent **))
+    __attribute__((__warn_unused_result__));
 
 #ifdef __cplusplus
 }

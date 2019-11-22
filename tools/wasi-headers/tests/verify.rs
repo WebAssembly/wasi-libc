95e16a669d31c3153ca411418eb7d65ff27f93fa
Dan Gohman <sunfish@mozilla.com>
2019-11-21 17:22:19 -0800

    Add a diff mechanism to help explain failures.

--- tools/wasi-headers/tests/verify.rs
+++ tools/wasi-headers/tests/verify.rs
@@ -5,14 +5,39 @@ fn assert_same_as_src() {
     if actual == expected {
         return;
     }
-    panic!(
-        "
 
-the generate `<wasi/api.h>` does not match the actual source `<wasi/api.h>`, it's
-recommended to run this command from the root of the repository:
+    eprintln!("The following diff was found between the generated <wasi/api.h> and the");
+    eprintln!("source <wasi/api.h> in the tree:");
+    eprintln!();
 
-    cd tools/wasi-headers && cargo run
+    let mut expected_line = 1;
+    let mut actual_line = 1;
+    let mut separated = false;
+    for diff in diff::lines(&expected, &actual) {
+        match diff {
+            diff::Result::Left(l) => {
+                eprintln!("line {}: -{}", expected_line, l);
+                expected_line += 1;
+                separated = false;
+            }
+            diff::Result::Both(_, _) => {
+                expected_line += 1;
+                actual_line += 1;
+                if !separated {
+                    eprintln!("...");
+                    separated = true;
+                }
+            }
+            diff::Result::Right(r) => {
+                eprintln!("line {}: +{}", actual_line, r);
+                actual_line += 1;
+                separated = false;
+            }
+        }
+    }
 
-"
-    );
+    eprintln!();
+    eprintln!("To regenerate the files, run `cd tools/wasi-headers && cargo run`.");
+    eprintln!();
+    panic!();
 }

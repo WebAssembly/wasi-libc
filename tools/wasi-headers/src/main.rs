c97a5006c8ef469a3db26f5381f544629c004e8f
Pat Hickey <phickey@fastly.com>
2020-01-14 12:35:27 -0800

    wasi-headers: restructure lib and exe to be more flexible
    just factor out some of the hard-coded stuff

--- tools/wasi-headers/src/main.rs
+++ tools/wasi-headers/src/main.rs
@@ -1,11 +1,60 @@
+#[macro_use]
+extern crate clap;
+
 use anyhow::Result;
+use clap::{Arg, SubCommand};
 use std::fs::File;
 use std::io::Write;
-use wasi_headers::generate;
+use std::path::PathBuf;
+use wasi_headers::{generate, libc_wasi_api_header, snapshot_witx_files};
 
-pub fn main() -> Result<()> {
-    let c_header = generate()?;
-    let mut file = File::create("../../libc-bottom-half/headers/public/wasi/api.h")?;
-    file.write_all(c_header.as_bytes())?;
+struct GenerateCommand {
+    /// Input witx file
+    inputs: Vec<PathBuf>,
+    /// Output header file
+    output: PathBuf,
+}
+
+impl GenerateCommand {
+    pub fn execute(&self) -> Result<()> {
+        let c_header = generate(&self.inputs)?;
+        let mut file = File::create(&self.output)?;
+        file.write_all(c_header.as_bytes())?;
+        Ok(())
+    }
+}
+
+fn main() -> Result<()> {
+    let matches = app_from_crate!()
+        .arg(Arg::with_name("inputs").required(true).multiple(true))
+        .arg(
+            Arg::with_name("output")
+                .short("o")
+                .long("output")
+                .takes_value(true)
+                .required(true),
+        )
+        .subcommand(
+            SubCommand::with_name("generate-libc")
+                .about("generate libc api.h from current snapshot"),
+        )
+        .get_matches();
+
+    let cmd = if matches.subcommand_matches("generate-libc").is_some() {
+        let inputs = snapshot_witx_files()?;
+        let output = libc_wasi_api_header();
+        GenerateCommand { inputs, output }
+    } else {
+        GenerateCommand {
+            inputs: matches
+                .values_of("inputs")
+                .expect("inputs required")
+                .map(PathBuf::from)
+                .collect(),
+            output: PathBuf::from(matches.value_of("output").expect("output required")),
+        }
+    };
+
+    cmd.execute()?;
     Ok(())
 }

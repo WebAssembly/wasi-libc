d6b1a2ba5a6662963b528ed9eca2e0bf9f04ade0
Dan Gohman <sunfish@mozilla.com>
2020-04-13 13:22:34 -0700

    Mark _activate and _start as wasm exports.

--- libc-bottom-half/crt/reactor-crt1.c
+++ libc-bottom-half/crt/reactor-crt1.c
@@ -1,5 +1,6 @@
 extern void __wasm_call_ctors(void);
 
+__attribute__((export_name("_activate")))
 void _activate(void) {
     // The linker synthesizes this to call constructors.
     __wasm_call_ctors();

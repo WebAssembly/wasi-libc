d31d8383aaa024c22f7e4a99df6468cfe13c6f28
Dan Gohman <dev@sunfishcode.online>
2020-10-01 09:57:02 -0700

    Add a `d_namlen` field to the public `dirent` struct.

--- libc-bottom-half/headers/public/__struct_dirent.h
+++ libc-bottom-half/headers/public/__struct_dirent.h
@@ -4,13 +4,23 @@
 #include <__typedef_ino_t.h>
 
 #define _DIRENT_HAVE_D_TYPE
+#define _DIRENT_HAVE_D_NAMLEN
 
 struct dirent {
     /// Similar to the traditional `d_off` field, however not compatible with
     /// `seekdir`. Use with `__wasilibc_seekdir` instead.
     unsigned long long d_loc;
+
+    /// The inode number of the entry.
     ino_t d_ino;
+
+    /// The length of the string in `d_name`, not including the trailing NUL.
+    unsigned d_namlen;
+
+    /// Type of filesystem entry (file, directory, etc.).
     unsigned char d_type;
+
+    /// The name of the entry, with a trailing NUL.
     char d_name[];
 };
 

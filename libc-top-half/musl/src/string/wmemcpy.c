38767df98daa5584bb24d50a21f33bba50eadd34
Cheng Shao <astrohavoc@gmail.com>
2021-12-04 03:56:45 +0000

    Use bulk memory opcodes when possible

--- libc-top-half/musl/src/string/wmemcpy.c
+++ libc-top-half/musl/src/string/wmemcpy.c
@@ -2,7 +2,11 @@
 
 wchar_t *wmemcpy(wchar_t *restrict d, const wchar_t *restrict s, size_t n)
 {
+#if defined(__wasm_bulk_memory__)
+	return __builtin_wmemcpy(d, s, n);
+#else
 	wchar_t *a = d;
 	while (n--) *d++ = *s++;
 	return a;
+#endif // __wasm_bulk_memory__
 }
--- libc-top-half/musl/src/string/wmemmove.c
+++ libc-top-half/musl/src/string/wmemmove.c
@@ -3,6 +3,9 @@
 
 wchar_t *wmemmove(wchar_t *d, const wchar_t *s, size_t n)
 {
+#if defined(__wasm_bulk_memory__)
+	return __builtin_wmemmove(d, s, n);
+#else
 	wchar_t *d0 = d;
 	if (d == s) return d;
 	if ((uintptr_t)d-(uintptr_t)s < n * sizeof *d)
@@ -10,4 +13,5 @@ wchar_t *wmemmove(wchar_t *d, const wchar_t *s, size_t n)
 	else
 		while (n--) *d++ = *s++;
 	return d0;
+#endif // __wasm_bulk_memory__
 }

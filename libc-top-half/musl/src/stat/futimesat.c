WebAssembly/wasi-libc

commit 079adff840032c3455eb1cb34dc9ceaa0b2bfc0c

Enable support for `utimes` and `futimesat`.
These can both be implemented in terms of `utimensat`.

libc-top-half/musl/src/stat/futimesat.c
@@ -2,7 +2,9 @@
 #include <sys/time.h>
 #include <sys/stat.h>
 #include <errno.h>
+#ifdef __wasilibc_unmodified_upstream // WASI has no syscall
 #include "syscall.h"
+#endif
 
 int __futimesat(int dirfd, const char *pathname, const struct timeval times[2])
 {
@@ -11,7 +13,12 @@ int __futimesat(int dirfd, const char *pathname, const struct timeval times[2])
 		int i;
 		for (i=0; i<2; i++) {
 			if (times[i].tv_usec >= 1000000ULL)
+#ifdef __wasilibc_unmodified_upstream // WASI has no syscall
 				return __syscall_ret(-EINVAL);
+#else
+				errno = EINVAL;
+				return -1;
+#endif
 			ts[i].tv_sec = times[i].tv_sec;
 			ts[i].tv_nsec = times[i].tv_usec * 1000;
 		}

Makefile
@@ -132,6 +132,7 @@ LIBC_TOP_HALF_MUSL_SOURCES = \
         env/setenv.c \
         env/unsetenv.c \
         unistd/posix_close.c \
+        stat/futimesat.c \
     ) \
     $(filter-out %/procfdname.c %/syscall.c %/syscall_ret.c %/vdso.c %/version.c, \
                  $(wildcard $(LIBC_TOP_HALF_MUSL_SRC_DIR)/internal/*.c)) \

libc-bottom-half/sources/posix.c
@@ -139,6 +139,28 @@ int utime(const char *path, const struct utimbuf *times) {
                      0);
 }
 
+int utimes(const char *path, const struct timeval times[2]) {
+    char *relative_path;
+    int dirfd = find_relpath(path, &relative_path);
+
+    // If we can't find a preopen for it, indicate that we lack capabilities.
+    if (dirfd == -1) {
+        errno = ENOTCAPABLE;
+        return -1;
+    }
+
+    return __wasilibc_nocwd_utimensat(
+             dirfd, relative_path,
+                     times ? ((struct timespec [2]) {
+                                 { .tv_sec = times[0].tv_sec,
+				   .tv_nsec = times[0].tv_usec * 1000 },
+                                 { .tv_sec = times[1].tv_sec,
+				   .tv_nsec = times[1].tv_usec * 1000 },
+                             })
+                           : NULL,
+                     0);
+}
+
 int unlink(const char *path) {
     char *relative_path;
     int dirfd = find_relpath(path, &relative_path);

libc-top-half/musl/include/sys/time.h
@@ -23,9 +23,7 @@ struct itimerval {
 int getitimer (int, struct itimerval *);
 int setitimer (int, const struct itimerval *__restrict, struct itimerval *__restrict);
 #endif
-#ifdef __wasilibc_unmodified_upstream /* WASI libc doesn't build the legacy functions */
 int utimes (const char *, const struct timeval [2]);
-#endif
 
 #if defined(_GNU_SOURCE) || defined(_BSD_SOURCE)
 struct timezone {
@@ -34,7 +32,9 @@ struct timezone {
 };
 #ifdef __wasilibc_unmodified_upstream /* WASI libc doesn't build the legacy functions */
 int futimes(int, const struct timeval [2]);
+#endif
 int futimesat(int, const char *, const struct timeval [2]);
+#ifdef __wasilibc_unmodified_upstream /* WASI libc doesn't build the legacy functions */
 int lutimes(const char *, const struct timeval [2]);
 #endif
 #ifdef __wasilibc_unmodified_upstream /* WASI has no way to set the time */

commit 18774980bf0a3715eb7d8d3f0eabfd3635016bff

Fix futimens error handling.
Fix a bug introduced in #242 noticed [here]: if `tv_usec` is out of
range, then properly return an error code.

[here]: https://github.com/WebAssembly/wasi-libc/commit/079adff840032c3455eb1cb34dc9ceaa0b2bfc0c#r69631767

libc-top-half/musl/src/stat/futimesat.c
@@ -12,12 +12,14 @@ int __futimesat(int dirfd, const char *pathname, const struct timeval times[2])
 	if (times) {
 		int i;
 		for (i=0; i<2; i++) {
-			if (times[i].tv_usec >= 1000000ULL)
 #ifdef __wasilibc_unmodified_upstream // WASI has no syscall
+			if (times[i].tv_usec >= 1000000ULL)
 				return __syscall_ret(-EINVAL);
 #else
+			if (times[i].tv_usec >= 1000000ULL) {
 				errno = EINVAL;
 				return -1;
+			}
 #endif
 			ts[i].tv_sec = times[i].tv_sec;
 			ts[i].tv_nsec = times[i].tv_usec * 1000;
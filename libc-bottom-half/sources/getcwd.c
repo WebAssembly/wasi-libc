0356da2c70da3f0e8ec07b81aac9b2be07cd49a7
Johnathan Sharratt <johnathan.sharratt@gmail.com>
2022-05-29 10:15:05 +0200

    Added getpid and fixed up chdir/getpwd

--- libc-bottom-half/sources/getcwd.c
+++ libc-bottom-half/sources/getcwd.c
@@ -2,46 +2,15 @@
 #include <errno.h>
 #include <string.h>
 
-#if LONG_MAX > INT_MAX
-
 char *getcwd(char *buf, size_t size)
 {
     // Perform system call.
-    uint64_t size_64 = size;
-    __wasi_errno_t error = __wasi_getcwd((uint8_t *)buf, &size_64);
+    __wasi_pointersize_t bufsize = size;
+    __wasi_errno_t error = __wasi_getcwd((uint8_t *)buf, &bufsize);
+    size = (size_t)bufsize;
     if (error != 0) {
       errno = error;
       return NULL;
     }
     return buf;
 }
-
-#else
-
-// For threads this needs to synchronize with chdir
-#ifdef _REENTRANT
-#error "getcwd doesn't yet support multiple threads"
-#endif
-
-char *__wasilibc_cwd = "/";
-
-char *getcwd(char *buf, size_t size)
-{
-    if (!buf) {
-        buf = strdup(__wasilibc_cwd);
-        if (!buf) {
-            errno = ENOMEM;
-            return NULL;
-        }
-    } else {
-        size_t len = strlen(__wasilibc_cwd);
-        if (size < len + 1) {
-            errno = ERANGE;
-            return NULL;
-        }
-        strcpy(buf, __wasilibc_cwd);
-    }
-    return buf;
-}
-
-#endif

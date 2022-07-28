9d3b1e4d11b64062aef2dff6b9646aa154166c46
Andrew Brown <andrew.brown@intel.com>
2022-07-28 09:42:49 -0700

    Rename `__wasi_libc_pthread_self` to `__wasilibc_pthread_self`
    The symbol is still undefined, though.

--- libc-top-half/musl/arch/wasm32/pthread_arch.h
+++ libc-top-half/musl/arch/wasm32/pthread_arch.h
@@ -2,7 +2,7 @@ uintptr_t __get_tp(void) {
 #if _REENTRANT
   int val;
   __asm__(".globaltype g_needs_dynamic_alloc, i32\n"
-          "global.get __wasi_libc_pthread_self\n"
+          "global.get __wasilibc_pthread_self\n"
           "local.set %0" : "=r" (val));
   return val;
 #else

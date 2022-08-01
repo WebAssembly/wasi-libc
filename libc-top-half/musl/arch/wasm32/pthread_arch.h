0878de78744f33e2df320e1ca604171b5e53e77d
Andrew Brown <andrew.brown@intel.com>
2022-08-01 15:25:04 -0700

    Remove `g_needs_dynamic_alloc` global

--- libc-top-half/musl/arch/wasm32/pthread_arch.h
+++ libc-top-half/musl/arch/wasm32/pthread_arch.h
@@ -1,9 +1,9 @@
 uintptr_t __get_tp(void) {
 #if _REENTRANT
   int val;
-  __asm__(".globaltype g_needs_dynamic_alloc, i32\n"
-          "global.get __wasilibc_pthread_self\n"
-          "local.set %0" : "=r" (val));
+  __asm__("global.get __wasilibc_pthread_self\n"
+          "local.set %0"
+          : "=r"(val));
   return val;
 #else
   return 0;

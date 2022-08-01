3784170dfc858785c504a06c928b3e2e338ad181
Andrew Brown <andrew.brown@intel.com>
2022-08-01 16:37:55 -0700

    review: add #ifdefs to `__pthread_setcancelstate`

--- libc-top-half/musl/src/thread/pthread_setcancelstate.c
+++ libc-top-half/musl/src/thread/pthread_setcancelstate.c
@@ -2,13 +2,13 @@
 
 int __pthread_setcancelstate(int new, int *old)
 {
-	#ifdef _REENTRANT
-	if (new > 2U) return EINVAL;
-	struct pthread *self = __pthread_self();
-	if (old) *old = self->canceldisable;
-	self->canceldisable = new;
-	#endif
-	return 0;
+#if defined(__wasilibc_unmodified_upstream) || defined(_REENTRANT)
+    if (new > 2U) return EINVAL;
+    struct pthread *self = __pthread_self();
+    if (old) *old = self->canceldisable;
+    self->canceldisable = new;
+#endif
+    return 0;
 }
 
 weak_alias(__pthread_setcancelstate, pthread_setcancelstate);

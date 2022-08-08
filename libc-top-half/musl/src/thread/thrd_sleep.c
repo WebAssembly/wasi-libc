1d6a27cf14898424d1bd8335e7c9627e4061b9ac
Andrew Brown <andrew.brown@intel.com>
2022-08-08 16:30:54 -0700

    review: alias __clock_nanosleep in bottom half

--- libc-top-half/musl/src/thread/thrd_sleep.c
+++ libc-top-half/musl/src/thread/thrd_sleep.c
@@ -7,11 +7,7 @@
 
 int thrd_sleep(const struct timespec *req, struct timespec *rem)
 {
-#ifdef __wasilibc_unmodified_upstream
 	int ret = -__clock_nanosleep(CLOCK_REALTIME, 0, req, rem);
-#else
-	int ret = -clock_nanosleep(CLOCK_REALTIME, 0, req, rem);
-#endif
 	switch (ret) {
 	case 0:      return 0;
 	case -EINTR: return -1; /* value specified by C11 */

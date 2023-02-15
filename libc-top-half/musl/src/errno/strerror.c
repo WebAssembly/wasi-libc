46c8b2bb8c0c2fb9daebea8e2c26c24d41605f3d
Johnathan Sharratt <johnathan.sharratt@gmail.com>
2023-02-15 16:00:29 +1100

    Added a fix so that negative errno do not cause a panic

--- libc-top-half/musl/src/errno/strerror.c
+++ libc-top-half/musl/src/errno/strerror.c
@@ -34,6 +34,7 @@ char *__strerror_l(int e, locale_t loc)
 	if (e==EDQUOT) e=0;
 	else if (e==EDQUOT_ORIG) e=EDQUOT;
 #endif
+	if (e < 0) e = -e;
 	if (e >= sizeof errmsgidx / sizeof *errmsgidx) e = 0;
 	s = (char *)&errmsgstr + errmsgidx[e];
 	return (char *)LCTRANS(s, LC_MESSAGES, loc);

97e6dc29a5b8c658e4fc420e5ec8cb7530262d6a
Johnathan Sharratt <johnathan.sharratt@gmail.com>
2023-03-06 10:29:59 +1100

    Fixed the expected symbols

--- take-expected-32.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash -e
-cp -f sysroot/share/wasm32-wasi/defined-symbols.txt expected/wasm32-wasi/defined-symbols.txt
-cp -f sysroot/share/wasm32-wasi/include-all.c expected/wasm32-wasi/include-all.c
-cp -f sysroot/share/wasm32-wasi/predefined-macros.txt expected/wasm32-wasi/predefined-macros.txt
-cp -f sysroot/share/wasm32-wasi/undefined-symbols.txt expected/wasm32-wasi/undefined-symbols.txt

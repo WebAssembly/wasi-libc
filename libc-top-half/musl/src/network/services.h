3d55a80ad48fba8754c4bf55806fdb25dcbe4121
wayne warren <wayne.warren.s@gmail.com>
2023-05-19 14:32:41 -0600

    services.c: fix type signature and definition of get_entry

--- libc-top-half/musl/src/network/services.h
+++ libc-top-half/musl/src/network/services.h
@@ -6,11 +6,12 @@
 typedef struct services_entry_s {
   char *name;
   struct service *svcs;
+  int len_svcs;
   UT_hash_handle hh;
 } services_entry_t;
 
 void init_services();
-struct service* get_entry(const char *);
+services_entry_t* get_entry(const char *);
 
 extern services_entry_t *services;
 

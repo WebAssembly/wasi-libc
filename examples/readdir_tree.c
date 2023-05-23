7bc9208483a9229ca822a2a77b8f4c46bc0cfc49
wayne warren <wayne.warren.s@gmail.com>
2023-05-23 10:33:28 -0600

    examples: make readdir_tree actually print tree of directory

--- examples/readdir_tree.c
+++ examples/readdir_tree.c
@@ -1,30 +1,64 @@
+#include <stdarg.h>
 #include <stdio.h>
 #include <sys/random.h>
 #include <unistd.h>
+#include <string.h>
 
 #include <dirent.h>
 
-
-int print_dir() {
-  return 0;
+void print(int lvl, const char *format, ...) {
+  for (int i = 0; i < lvl; i++) {
+    printf("  ");
+  }
+  va_list args;
+  va_start(args, format);
+  vprintf(format, args);
+  va_end(args);
+  fflush(stdout);
 }
 
-int main() {
-  char *name = "/usr";
-  printf("main: just before opening dir '%s'\n", name);
-  fflush(stdout);
-  DIR *d = opendir(name);
-  printf("main: just after opening dir '%s'\n", name);
-  fflush(stdout);
+int handle_dir(int lvl, DIR *d) {
+  DIR *new;
+  int ret = 0;
   for (;;) {
-    printf("main: top of for loop\n");
-    fflush(stdout);
     struct dirent *ent = readdir(d);
     if (ent == NULL) {
-      break;
+      return 0;
     }
-    printf("main: dir entry name '%s'\n", ent->d_name);
-    fflush(stdout);
+
+    if (strcmp(ent->d_name, "..") == 0 || strcmp(ent->d_name, ".") == 0) {
+      continue;
+    }
+
+    switch (ent->d_type) {
+    case DT_DIR:
+      print(lvl, "%s:\n", ent->d_name);
+      new = opendir(ent->d_name);
+      ret = handle_dir(lvl + 1, new);
+      if (ret < 0) {
+        return ret;
+      }
+      continue;
+    default:
+      print(lvl, "%s\n", ent->d_name);
+    }
+  }
+
+  return ret;
+}
+
+int main(int argc, char *argv[]) {
+  if (argc < 1) {
+    fprintf(stderr, "not enough args!");
+  }
+  for (int i = 1; i < argc; i++) {
+    print(0, "%s\n", argv[i]);
+
+    DIR *d = opendir(argv[i]);
+    int ret = handle_dir(1, d);
+    if (ret < 0) {
+      return ret;
+    }
   }
   return 0;
 }

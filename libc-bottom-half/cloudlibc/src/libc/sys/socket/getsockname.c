2135b66f45d7d92e32413bec426e9907d04f0fc1
ptitSeb <sebastien.chev@gmail.com>
2023-08-17 16:19:27 +0200

    Changed getsockname to allow handling of pipe fd

--- libc-bottom-half/cloudlibc/src/libc/sys/socket/getsockname.c
+++ libc-bottom-half/cloudlibc/src/libc/sys/socket/getsockname.c
@@ -10,6 +10,12 @@
 int getsockname(int socket, struct sockaddr *restrict addr, socklen_t *restrict addrlen) {
   __wasi_addr_port_t local_addr;
   __wasi_errno_t error = __wasi_sock_addr_peer(socket, &local_addr);
+  if (error == ENOTSOCK) {
+    // in that case, return a AF_LOCAL type constant
+    *addrlen = sizeof(addr->sa_family);
+    addr->sa_family = AF_LOCAL;
+    return 0;
+  }
   if (error != 0) {
     errno = error;
     return -1;

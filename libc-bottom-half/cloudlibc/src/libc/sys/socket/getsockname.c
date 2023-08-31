9d5d8bbb58ea5b8d388f102c38febc7089fce9c4
Arshia Ghafoori <arshia001@live.com>
2023-08-31 12:12:53 +0400

    Fix getsockname calling sock_addr_peer instead of sock_addr_local

--- libc-bottom-half/cloudlibc/src/libc/sys/socket/getsockname.c
+++ libc-bottom-half/cloudlibc/src/libc/sys/socket/getsockname.c
@@ -9,7 +9,7 @@
 
 int getsockname(int socket, struct sockaddr *restrict addr, socklen_t *restrict addrlen) {
   __wasi_addr_port_t local_addr;
-  __wasi_errno_t error = __wasi_sock_addr_peer(socket, &local_addr);
+  __wasi_errno_t error = __wasi_sock_addr_local(socket, &local_addr);
   if (error == ENOTSOCK) {
     // in that case, return a AF_LOCAL type constant
     *addrlen = sizeof(addr->sa_family);

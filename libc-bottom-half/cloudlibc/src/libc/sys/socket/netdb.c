253f89597d6501460fc3f855a419e0aa09b571ce
Joel Dice <joel.dice@fermyon.com>
2023-12-06 17:26:17 -0700

    add netdb.h-related stubs
    Signed-off-by: Joel Dice <joel.dice@fermyon.com>

--- libc-bottom-half/cloudlibc/src/libc/sys/socket/netdb.c
+++ libc-bottom-half/cloudlibc/src/libc/sys/socket/netdb.c
@@ -1,14 +1,58 @@
-#include <stdlib.h>
 #include <netdb.h>
+#include <stdlib.h>
 
-int getaddrinfo(const char *restrict host, const char *restrict serv, const struct addrinfo *restrict hint, struct addrinfo **restrict res)
+_Thread_local int h_errno = 0;
+
+int getaddrinfo(const char* restrict host, const char* restrict serv, const struct addrinfo* restrict hint, struct addrinfo** restrict res)
 {
     // TODO wasi-sockets
     abort();
 }
 
-void freeaddrinfo(struct addrinfo *p)
+void freeaddrinfo(struct addrinfo* p)
+{
+    // TODO
+    abort();
+}
+
+int getnameinfo(const struct sockaddr* restrict sa, socklen_t salen, char* restrict host, socklen_t hostlen, char* restrict serv, socklen_t servlen, int flags)
 {
     // TODO wasi-sockets
+    abort();
+}
+
+struct hostent* gethostbyname(const char* name)
+{
+    // TODO
+    abort();
+}
+
+struct hostent* gethostbyaddr(const void* addr, socklen_t len, int type)
+{
+    // TODO
+    abort();
+}
+
+const char* hstrerror(int err)
+{
+    // TODO
+    abort();
+}
+
+struct servent* getservbyname(const char* name, const char* proto)
+{
+    // TODO
+    abort();
+}
+
+struct servent* getservbyport(int port, const char* proto)
+{
+    // TODO
+    abort();
+}
+
+struct protoent* getprotobyname(const char* name)
+{
+    // TODO
     abort();
 }

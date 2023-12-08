9e9171f5a37a0eaf5e23814cf47d882e8b5067e5
Dave Bakker <github@davebakker.io>
2023-12-08 20:44:26 +0100

    UDP send and recv

--- libc-bottom-half/cloudlibc/src/libc/sys/socket/recv.c
+++ libc-bottom-half/cloudlibc/src/libc/sys/socket/recv.c
@@ -18,8 +18,7 @@ ssize_t tcp_recvfrom(tcp_socket_t* socket, uint8_t* buffer, size_t length, int f
     // - MSG_WAITALL: we can probably support these relatively easy.
     // - MSG_OOB: could be shimmed by always responding that no OOB data is available.
     // - MSG_PEEK: could be shimmed by performing the receive into a local socket-specific buffer. And on subsequent receives first check that buffer.
-    // - MSG_TRUNC: return EOPNOTSUPP. Is UDP only.
-    // - MSG_EOR, MSG_CMSG_CLOEXEC: return EOPNOTSUPP. Is not supported on TCP or UDP.
+
     const int supported_flags = MSG_DONTWAIT;
     if ((flags & supported_flags) != flags) {
         errno = EOPNOTSUPP;
@@ -49,7 +48,7 @@ ssize_t tcp_recvfrom(tcp_socket_t* socket, uint8_t* buffer, size_t length, int f
         streams_list_u8_t result;
         streams_stream_error_t error;
         if (!streams_method_input_stream_read(rx_borrow, length, &result, &error)) {
-            // TODO wasi-sockets: wasi-sockets has no way to recover stream errors yet.
+            // TODO wasi-sockets: wasi-sockets has no way to recover TCP stream errors yet.
             errno = EPIPE;
             return -1;
         }
@@ -71,10 +70,85 @@ ssize_t tcp_recvfrom(tcp_socket_t* socket, uint8_t* buffer, size_t length, int f
 ssize_t udp_recvfrom(udp_socket_t* socket, uint8_t* buffer, size_t length, int flags,
     struct sockaddr* addr , socklen_t* addrlen)
 {
-    // TODO wasi-sockets: Implement flags. Same as tcp_recv except that MSG_TRUNC is valid for UDP.
+    // TODO wasi-sockets: flags:
+    // - MSG_PEEK: could be shimmed by performing the receive into a local socket-specific buffer. And on subsequent receives first check that buffer.
 
-    errno = EOPNOTSUPP;
-    return -1;
+    const int supported_flags = MSG_DONTWAIT | MSG_TRUNC;
+    if ((flags & supported_flags) != flags) {
+        errno = EOPNOTSUPP;
+        return -1;
+    }
+
+    output_sockaddr_t output_addr;
+    if (!__wasi_sockets_utils__output_addr_validate(socket->family, addr, addrlen, &output_addr)) {
+        errno = EINVAL;
+        return -1;
+    }
+
+    
+    network_error_code_t error;
+    udp_borrow_udp_socket_t socket_borrow = udp_borrow_udp_socket(socket->socket);
+
+    udp_socket_streams_t streams;
+    switch (socket->state.tag) {
+    case UDP_SOCKET_STATE_UNBOUND: {
+        // Unlike `send`, `recv` should _not_ perform an implicit bind.
+        errno = EINVAL;
+        return -1;
+    }
+    case UDP_SOCKET_STATE_BOUND_NOSTREAMS: {
+        if (!__wasi_sockets_utils__stream(socket, NULL, &streams, &error)) {
+            errno = __wasi_sockets_utils__map_error(error);
+            return -1;
+        }
+        break;
+    }
+    case UDP_SOCKET_STATE_BOUND_STREAMING:
+        streams = socket->state.bound_streaming.streams;
+        break;
+
+    case UDP_SOCKET_STATE_CONNECTED:
+        streams = socket->state.connected.streams;
+        break;
+
+    default: /* unreachable */ abort();
+    }
+
+    bool return_real_size = (flags & MSG_TRUNC) != 0;
+    bool should_block = socket->blocking;
+    if ((flags & MSG_DONTWAIT) != 0) {
+        should_block = false;
+    }
+
+    udp_borrow_incoming_datagram_stream_t incoming_borrow = udp_borrow_incoming_datagram_stream(streams.incoming);
+    while (true) {
+        udp_list_incoming_datagram_t datagrams;
+        if (!udp_method_incoming_datagram_stream_receive(incoming_borrow, 1, &datagrams, &error)) {
+            errno = __wasi_sockets_utils__map_error(error);
+            return -1;
+        }
+
+        if (datagrams.len) {
+            udp_incoming_datagram_t datagram = datagrams.ptr[0];
+            size_t datagram_size = datagram.data.len;
+            size_t bytes_to_copy = datagram_size < length ? datagram_size : length;
+
+            if (output_addr.tag != OUTPUT_SOCKADDR_NULL) {
+                __wasi_sockets_utils__output_addr_write(datagram.remote_address, &output_addr);
+            }
+
+            memcpy(buffer, datagram.data.ptr, bytes_to_copy);
+            udp_list_incoming_datagram_free(&datagrams);
+            return return_real_size ? datagram_size : bytes_to_copy;
+
+        } else if (should_block) {
+            poll_borrow_pollable_t pollable_borrow = poll_borrow_pollable(streams.incoming_pollable);
+            poll_method_pollable_block(pollable_borrow);
+        } else {
+            errno = EWOULDBLOCK;
+            return -1;
+        }
+    }
 }
 
 ssize_t recv(int socket, void* restrict buffer, size_t length, int flags) {
--- libc-bottom-half/cloudlibc/src/libc/sys/socket/send.c
+++ libc-bottom-half/cloudlibc/src/libc/sys/socket/send.c
@@ -58,7 +58,7 @@ ssize_t tcp_sendto(tcp_socket_t* socket, const uint8_t* buffer, size_t length, i
             count = count < length ? count : length;
             streams_list_u8_t list = { .ptr = (uint8_t*)buffer, .len = count };
             if (!streams_method_output_stream_write(tx_borrow, &list, &error)) {
-                // TODO wasi-sockets: wasi-sockets has no way to recover stream errors yet.
+                // TODO wasi-sockets: wasi-sockets has no way to recover TCP stream errors yet.
                 errno = EPIPE;
                 return -1;
             } else {
@@ -77,10 +77,126 @@ ssize_t tcp_sendto(tcp_socket_t* socket, const uint8_t* buffer, size_t length, i
 ssize_t udp_sendto(udp_socket_t* socket, const uint8_t* buffer, size_t length, int flags,
     const struct sockaddr* addr, socklen_t addrlen)
 {
-    // TODO wasi-sockets: Implement flags. Same as tcp_send.
+    const int supported_flags = MSG_DONTWAIT;
+    if ((flags & supported_flags) != flags) {
+        errno = EOPNOTSUPP;
+        return -1;
+    }
 
-    errno = EOPNOTSUPP;
-    return -1;
+    network_ip_socket_address_t remote_address;
+    bool has_remote_address = (addr != NULL);
+
+    if (has_remote_address) {
+        if (socket->state.tag == UDP_SOCKET_STATE_CONNECTED) {
+            errno = EISCONN;
+            return -1;
+        }
+
+        int parse_err;
+        if (!__wasi_sockets_utils__parse_address(socket->family, addr, addrlen, &remote_address, &parse_err)) {
+            errno = parse_err;
+            return -1;
+        }
+    } else {
+        if (addrlen != 0) {
+            errno = EINVAL;
+            return -1;
+        }
+
+        if (socket->state.tag != UDP_SOCKET_STATE_CONNECTED) {
+            errno = EDESTADDRREQ;
+            return -1;
+        }
+    }
+
+    network_error_code_t error;
+    udp_borrow_udp_socket_t socket_borrow = udp_borrow_udp_socket(socket->socket);
+
+    udp_socket_streams_t streams;
+    switch (socket->state.tag) {
+    case UDP_SOCKET_STATE_UNBOUND: {
+        // Socket is not explicitly bound by the user. We'll do it for them:
+
+        network_ip_socket_address_t any = __wasi_sockets_utils__any_addr(socket->family);
+        int result = __wasi_sockets_utils__udp_bind(socket, &any);
+        if (result != 0) {
+            return result;
+        }
+
+        if (!__wasi_sockets_utils__stream(socket, NULL, &streams, &error)) {
+            errno = __wasi_sockets_utils__map_error(error);
+            return -1;
+        }
+        break;
+    }
+    case UDP_SOCKET_STATE_BOUND_NOSTREAMS: {
+        if (!__wasi_sockets_utils__stream(socket, NULL, &streams, &error)) {
+            errno = __wasi_sockets_utils__map_error(error);
+            return -1;
+        }
+        break;
+    }
+    case UDP_SOCKET_STATE_BOUND_STREAMING:
+        streams = socket->state.bound_streaming.streams;
+        break;
+
+    case UDP_SOCKET_STATE_CONNECTED:
+        streams = socket->state.connected.streams;
+        break;
+
+    default: /* unreachable */ abort();
+    }
+
+    bool should_block = socket->blocking;
+    if ((flags & MSG_DONTWAIT) != 0) {
+        should_block = false;
+    }
+
+    udp_outgoing_datagram_t datagrams[1] = {{
+        .remote_address = {
+            .is_some = has_remote_address,
+            .val = remote_address,
+        },
+        .data = {
+            .len = length,
+            .ptr = (uint8_t*)buffer,
+        },
+    }};
+    udp_list_outgoing_datagram_t list = {
+        .len = 1,
+        .ptr = datagrams,
+    };
+
+    udp_borrow_outgoing_datagram_stream_t outgoing_borrow = udp_borrow_outgoing_datagram_stream(streams.outgoing);
+    while (true) {
+        uint64_t allowed;
+        if (!udp_method_outgoing_datagram_stream_check_send(outgoing_borrow, &allowed, &error)) {
+            errno = __wasi_sockets_utils__map_error(error);
+            return -1;
+        }
+
+        if (allowed) {
+            uint64_t datagrams_sent;
+            if (!udp_method_outgoing_datagram_stream_send(outgoing_borrow, &list, &datagrams_sent, &error)) {
+                errno = __wasi_sockets_utils__map_error(error);
+                return -1;
+            }
+
+            assert(datagrams_sent == 0 || datagrams_sent == 1);
+
+            if (datagrams_sent == 1) {
+                return length;
+            }
+        }
+        
+        if (should_block) {
+            poll_borrow_pollable_t pollable_borrow = poll_borrow_pollable(streams.outgoing_pollable);
+            poll_method_pollable_block(pollable_borrow);
+        } else {
+            errno = EWOULDBLOCK;
+            return -1;
+        }
+    }
 }
 
 ssize_t send(int socket, const void* buffer, size_t length, int flags) {

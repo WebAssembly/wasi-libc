c889960bf8f0aef4db31915d203a835065569beb
trcrsired <uwgghhbcad@gmail.com>
2023-12-14 12:48:53 -0500

    change _i64 postfix to _wasm64

--- libc-bottom-half/sources/__wasilibc_real.c
+++ libc-bottom-half/sources/__wasilibc_real.c
@@ -14,7 +14,7 @@
 #include <string.h>
 
 #ifdef __wasm64__
-#define IMPORT_NAME(x) __import_name__(x "_i64")
+#define IMPORT_NAME(x) __import_name__(x "_wasm64")
 #else
 #define IMPORT_NAME(x) __import_name__(x)
 #endif

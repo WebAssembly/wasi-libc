7ea3949cbf23a33077de718e0191cfaafac78f35
R <rqou@berkeley.edu>
2024-09-25 18:16:25 +0100

    Add a stub for pthread_once

--- stub-pthreads/stub-pthreads-good.c
+++ stub-pthreads/stub-pthreads-good.c
@@ -7,3 +7,12 @@ static void dummy_0()
 }
 weak_alias(dummy_0, __acquire_ptc);
 weak_alias(dummy_0, __release_ptc);
+
+int pthread_once(pthread_once_t *control, void (*init)(void))
+{
+	if (!*control) {
+		init();
+		*control = 1;
+	}
+	return 0;
+}

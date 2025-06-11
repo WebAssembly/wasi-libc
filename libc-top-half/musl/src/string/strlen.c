f6c12e2433747488f24fd187f5df7358d5a7acad
Nuno Cruces <ncruces@users.noreply.github.com>
2025-06-11 16:05:23 +0100

    Optional SIMD strlen

--- libc-top-half/musl/src/string/strlen.c
+++ libc-top-half/musl/src/string/strlen.c
@@ -1,6 +1,7 @@
 #include <string.h>
 #include <stdint.h>
 #include <limits.h>
+#include <wasm_simd128.h>
 
 #define ALIGN (sizeof(size_t))
 #define ONES ((size_t)-1/UCHAR_MAX)
@@ -9,6 +10,32 @@
 
 size_t strlen(const char *s)
 {
+#if defined(__wasm_simd128__) && defined(__wasilibc_simd_string)
+  // strlen must stop as soon as it finds the terminator.
+  // Aligning ensures loads beyond the terminator are safe.
+  uintptr_t align = (uintptr_t)s % sizeof(v128_t);
+  const v128_t *v = (v128_t *)(s - align);
+
+  for (;;) {
+    // Bitmask is slow on AArch64, all_true is much faster.
+    if (!wasm_i8x16_all_true(*v)) {
+      const v128_t cmp = wasm_i8x16_eq(*v, (v128_t){});
+      // Clear the bits corresponding to alignment (little-endian)
+      // so we can count trailing zeros.
+      int mask = wasm_i8x16_bitmask(cmp) >> align << align;
+      // At least one bit will be set, unless we cleared them.
+      // Knowing this helps the compiler.
+      __builtin_assume(mask || align);
+      if (mask) {
+        // Find the offset of the first one bit (little-endian).
+        return (char *)v - s + __builtin_ctz(mask);
+      }
+    }
+    align = 0;
+    v++;
+  }
+#endif
+
 	const char *a = s;
 #ifdef __GNUC__
 	typedef size_t __attribute__((__may_alias__)) word;

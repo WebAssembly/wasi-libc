76098064dd4614046d86dd5b03f6914bb2fb64c6
Nuno Cruces <ncruces@users.noreply.github.com>
2025-06-13 12:22:09 +0100

    Add test.

---
+++ test/src/misc/strlen.c
@@ -0,0 +1,36 @@
+//! add-flags.py(LDFLAGS): -Wl,--stack-first -Wl,--initial-memory=327680
+
+static char *const LIMIT = (char *)327680;
+
+#include <__macro_PAGESIZE.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+void test(char *ptr, size_t want) {
+  size_t got = strlen(ptr);
+  if (got != want) {
+    printf("strlen(%p) = %lu, want %lu\n", ptr, got, want);
+  }
+}
+
+int main(void) {
+  for (size_t length = 0; length < 64; length++) {
+    for (size_t alignment = 0; alignment < 24; alignment++) {
+      char *ptr = LIMIT - PAGESIZE + alignment;
+      memset(LIMIT - 2 * PAGESIZE, 0, 2 * PAGESIZE);
+      memset(ptr, 5, length);
+      test(ptr, length);
+
+      ptr[-1] = 5;
+      test(ptr, length);
+    }
+
+    char *ptr = LIMIT - length - 1;
+    memset(LIMIT - 2 * PAGESIZE, 0, 2 * PAGESIZE);
+    memset(ptr, 5, length);
+    test(ptr, length);
+  }
+
+  return 0;
+}

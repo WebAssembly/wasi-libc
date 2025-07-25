39e65a4c6f9ff84f9bc8ff3bc83f3061e81b23b2
Tim Chevalier <tjc@igalia.com>
2025-07-24 19:38:29 -0700

    Port clocks and filesystems functions to use native wasip2 operations
    At this point, there should be no further uses of the preview1
    component adapter for clocks and filesystems methods when __wasilibc_use_wasip2
    is defined.
    
    Also added a DEBUG option to the Makefile that enables -O0/g
    when DEBUG=true is passed in.

--- libc-bottom-half/cloudlibc/src/libc/sys/time/gettimeofday.c
+++ libc-bottom-half/cloudlibc/src/libc/sys/time/gettimeofday.c
@@ -6,13 +6,23 @@
 
 #include <sys/time.h>
 
+#ifdef __wasilibc_use_wasip2
+#include <wasi/wasip2.h>
+#else
 #include <wasi/api.h>
+#endif
 
 int gettimeofday(struct timeval *restrict tp, void *tz) {
   if (tp != NULL) {
+#ifdef __wasilibc_use_wasip2
+    wall_clock_datetime_t time_result;
+    wall_clock_now(&time_result);
+    *tp = timestamp_to_timeval(&time_result);
+#else
     __wasi_timestamp_t ts = 0;
     (void)__wasi_clock_time_get(__WASI_CLOCKID_REALTIME, 1000, &ts);
     *tp = timestamp_to_timeval(ts);
+#endif
   }
   return 0;
 }
--- libc-bottom-half/cloudlibc/src/libc/unistd/fsync.c
+++ libc-bottom-half/cloudlibc/src/libc/unistd/fsync.c
@@ -2,15 +2,37 @@
 //
 // SPDX-License-Identifier: BSD-2-Clause
 
+#ifdef __wasilibc_use_wasip2
+#include <wasi/wasip2.h>
+#include <wasi/file_utils.h>
+#include <common/errors.h>
+#else
 #include <wasi/api.h>
+#endif
 #include <errno.h>
 #include <unistd.h>
 
 int fsync(int fildes) {
+#ifdef __wasilibc_use_wasip2
+  // Translate the file descriptor to an internal handle
+  filesystem_borrow_descriptor_t file_handle;
+  if (!fd_to_file_handle_allow_open(fildes, &file_handle)) {
+    errno = EBADF;
+    return -1;
+  }
+
+  // Sync the file
+  filesystem_error_code_t error_code;
+  if (!filesystem_method_descriptor_sync(file_handle, &error_code)) {
+    translate_error(error_code);
+    return -1;
+  }
+#else
   __wasi_errno_t error = __wasi_fd_sync(fildes);
   if (error != 0) {
     errno = error == ENOTCAPABLE ? EINVAL : error;
     return -1;
   }
+#endif
   return 0;
 }

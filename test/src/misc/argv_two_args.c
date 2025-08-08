2edeb565e571550f345b05164e28b740075bc4ee
Tim Chevalier <tjc@igalia.com>
2025-08-07 17:40:04 -0700

    Update test/src/misc/argv_two_args.c
    Co-authored-by: Pat Hickey <p.hickey@f5.com>

--- test/src/misc/argv_two_args.c
+++ test/src/misc/argv_two_args.c
@@ -10,7 +10,7 @@
 int main(int argc, char **argv)
 {
         char buf[PATH_MAX];
-        TEST(argc == 3, "argc should be 2\n");
+        TEST(argc == 3, "argc should be 3\n");
         TEST(argv[0] != 0, "argv[0] should not be NULL\n");
         TEST(argv[1] != 0, "argv[1] should not be NULL\n");
         TEST(argv[2] != 0, "argv[2] should not be NULL\n");

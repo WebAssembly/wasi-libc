261897d2e5621b175efab6ef4d449db7df121061
Tim Chevalier <tjc@igalia.com>
2025-08-08 16:57:04 -0700

    Port time() and times()

--- libc-bottom-half/cloudlibc/src/libc/time/time.c
+++ libc-bottom-half/cloudlibc/src/libc/time/time.c
@@ -4,12 +4,22 @@
 
 #include <common/time.h>
 
+#ifdef __wasilibc_use_wasip2
+#include <wasi/wasip2.h>
+#else
 #include <wasi/api.h>
+#endif
 #include <time.h>
 
 time_t time(time_t *tloc) {
+#ifdef __wasilibc_use_wasip2
+  wall_clock_datetime_t res;
+  wall_clock_now(&res);
+  uint64_t ts = (res.seconds * NSEC_PER_SEC) + res.nanoseconds;
+#else
   __wasi_timestamp_t ts = 0;
   (void)__wasi_clock_time_get(__WASI_CLOCKID_REALTIME, NSEC_PER_SEC, &ts);
+#endif
   if (tloc != NULL)
     *tloc = ts / NSEC_PER_SEC;
   return ts / NSEC_PER_SEC;

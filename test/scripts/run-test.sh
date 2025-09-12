6daeec8dfccfc52c0e3decf5b320ddb6f6325567
Tim Chevalier <tjc@igalia.com>
2025-09-11 18:33:56 -0700

    Add a simple test for TCP sockets
     Add two test programs, sockets-client.c and sockets-server.c, that
     are run by the test driver as separate executables (since without
     threads, this can't be written as a single test).

--- test/scripts/run-test.sh
+++ test/scripts/run-test.sh
@@ -12,6 +12,40 @@ ENGINE="${ENGINE:-wasmtime}"
 [ -n "$WASM" ] || (echo "missing WASM variable" && exit 1)
 [ -n "$DIR" ] || (echo "missing DIR variable" && exit 1)
 
+# Run the client/server sockets test, which requires a client and server
+# running in separate processes
+run_sockets_test() {
+    # Args are the same for client and server
+    cd $DIR
+    server_wasm=`echo $WASM | sed -e 's/client/server/g'`
+    echo "$ENV $ENGINE $RUN $server_wasm $ARGS" > server_cmd.sh
+    chmod +x server_cmd.sh
+    # Start the server
+    ./server_cmd.sh &> server_output.log &
+    PID=$!
+    [ $? -ne -1 ] || (echo "Failed to start server $server_wasm" && exit 1)
+    echo "$ENV $ENGINE $RUN $WASM $ARGS" > cmd.sh
+    chmod +x cmd.sh
+    # Allow time for the server to start
+    sleep 1
+    # Start the client
+    ./cmd.sh &> output.log
+    test_result=$?
+    # Server normally exits on its own, but kill it in case the test failed
+    if [ ps -p $PID > /dev/null 2>&1 ]; then
+        kill -9 $PID
+    fi
+    [ $test_result -eq 0 ] || echo "Test failed" >> output.log
+}
+
+testname=$(basename $WASM)
+if [ $testname == "sockets-server.component.wasm" ]; then
+    exit 0
+fi
+if [ $testname == "sockets-client.component.wasm" ]; then
+    run_sockets_test
+    exit $?
+fi
 cd $DIR
 mkdir -p fs
 echo "$ENV $ENGINE $RUN $WASM $ARGS" > cmd.sh

a45457d2540f16f2f1d9d4b809d9688f715f4ee0
Alex Crichton <alex@alexcrichton.com>
2025-11-11 22:04:30 -0800

    wasip2: Fix implementation of readv
    The implementation must update the file cursor position so `preadv`
    isn't an appropriate implementation. Find a non-empty iovec and then
    delegate to `read`.

---
+++ test/src/readv.c
@@ -0,0 +1,79 @@
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/uio.h>
+#include "test.h"
+
+#define TEST(c) do { \
+	errno = 0; \
+	if (!(c)) \
+		t_error("%s failed (errno = %d)\n", #c, errno); \
+} while(0)
+
+int main(void) {
+  char tmp[] = "testsuite-XXXXXX";
+  int fd;
+  TEST((fd = open(tmp, O_RDWR | O_CREAT | O_EXCL, 0600)) > 2);
+
+  TEST(write(fd, "Hello, world!", 13) == 13);
+  TEST(lseek(fd, 0, SEEK_SET) == 0);
+
+  char buf[10];
+
+  struct iovec iovs[3];
+  iovs[0].iov_base = NULL;
+  iovs[0].iov_len = 0;
+  iovs[1].iov_base = buf;
+  iovs[1].iov_len = sizeof(buf);
+  TEST(readv(fd, iovs, 2) == 10);
+  TEST(readv(fd, iovs, 2) == 3);
+  TEST(lseek(fd, 0, SEEK_CUR) == 13);
+
+  TEST(lseek(fd, 0, SEEK_SET) == 0);
+  iovs[0].iov_base = buf;
+  iovs[0].iov_len = sizeof(buf);
+  iovs[1].iov_base = NULL;
+  iovs[1].iov_len = 0;
+  TEST(readv(fd, iovs, 2) == 10);
+  TEST(readv(fd, iovs, 2) == 3);
+  TEST(lseek(fd, 0, SEEK_CUR) == 13);
+
+  TEST(lseek(fd, 0, SEEK_SET) == 0);
+  iovs[0].iov_base = NULL;
+  iovs[0].iov_len = 0;
+  iovs[1].iov_base = NULL;
+  iovs[1].iov_len = 0;
+  iovs[2].iov_base = buf;
+  iovs[2].iov_len = sizeof(buf);
+  TEST(readv(fd, iovs, 3) == 10);
+  TEST(readv(fd, iovs, 3) == 3);
+  TEST(lseek(fd, 0, SEEK_CUR) == 13);
+
+  TEST(lseek(fd, 0, SEEK_SET) == 0);
+  iovs[0].iov_base = buf;
+  iovs[0].iov_len = 7;
+  iovs[1].iov_base = buf;
+  iovs[1].iov_len = 7;
+  TEST(readv(fd, iovs, 2) >= 0);
+  TEST(readv(fd, iovs, 2) >= 0);
+  TEST(readv(fd, iovs, 2) == 0);
+  TEST(lseek(fd, 0, SEEK_CUR) == 13);
+
+  TEST(lseek(fd, 0, SEEK_SET) == 0);
+  iovs[0].iov_base = NULL;
+  iovs[0].iov_len = 0;
+  iovs[1].iov_base = NULL;
+  iovs[1].iov_len = 0;
+  TEST(readv(fd, iovs, 2) == 0);
+  TEST(readv(fd, iovs, 2) == 0);
+  TEST(lseek(fd, 0, SEEK_CUR) == 0);
+
+  close(fd);
+
+  return t_status;
+}
+

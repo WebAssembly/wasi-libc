688a685d1c5037ef8d718b9d3f91a4063c3ace77
Alex Crichton <alex@alexcrichton.com>
2025-12-18 10:44:33 -0600

    Migrate the `Makefile` to CMake (#685)
    This commit migrates wasi-libc's build system from a `make`-based system
    to CMake. This is a complete rewrite of the build system which
    culminates in the deletion of the current `Makefile` and a few
    supporting scripts and files.
    
    The rationale/reasons for this are similar to WebAssembly/wasi-sdk#429,
    namely:
    
    * Building a correct and robust build system in `make` is not easy.
    There are many times I've found myself in a situation where I need to
    blow away the entire build directory between builds. Much of the this
    this bottoms out in subtle behavior like "this file was renamed, but
    didn't get deleted in the archive" or subtle things like that. CMake is
    responsible for handling these by default and, in general, is probably
    going to be more correct than what we write.
    
    * Out-of-tree builds are now supported.
    
    * Customizing CFLAGS is now supported via standard mechanisms.
    Previously `EXTRA_CFLAGS` was required since using `CFLAGS` could break
    the build.
    
    * It's easier to move more logic into CMake, such as downloading
    compiler-rt, than it is to codify it all in makefiles.
    
    * Platform portability is generally easier in CMake than make. Building
    on Windows shouldn't require a full GNU-like environment, for example.
    
    * Tests now properly rebuild themselves when wasi-libc changes.
    
    * It's easier to customize high-level options, like "enable SIMD", in
    CMake than it is in Makefiles. This can be documented as a single option
    to pass where that option affects the build, flags, etc.
    
    Personally I'm not a fan of CMake, but I'm more of a fan of it than
    Makefiles, hence my desire to switch. I want to make this repository
    easier to build, configure, and change over time. This will also make it
    easier to integrate this all into wasi-sdk where everything is
    CMake-based over there as well.
    
    I am not a CMake expert, nor am I necessarily an expert in the previous
    Makefiles. I've done my best here, but I'm happy to change things if
    someone who knows more about CMake than I (which is a lot of folks)
    recommends doing so. I'm also happy to adjust the libc build as desired
    too.
    
    Closes #46
    Closes #156
    Closes #259
    Closes #322
    Closes #330
    Closes #514
    Closes #551
    Closes #605

---
+++ cmake/bindings.cmake
@@ -0,0 +1,66 @@
+include(ba-download)
+
+ba_download(
+  wit-bindgen
+  "https://github.com/bytecodealliance/wit-bindgen"
+  "0.48.0"
+)
+ExternalProject_Get_Property(wit-bindgen SOURCE_DIR)
+set(wit_bindgen "${SOURCE_DIR}/wit-bindgen")
+
+ExternalProject_Add(
+  wasi-wits
+  URL https://github.com/WebAssembly/wasi-cli/archive/refs/tags/v0.2.0.tar.gz
+  CONFIGURE_COMMAND ""
+  BUILD_COMMAND ""
+  INSTALL_COMMAND ""
+  EXCLUDE_FROM_ALL TRUE
+)
+ExternalProject_Get_Property(wasi-wits SOURCE_DIR)
+set(wit_dir ${SOURCE_DIR}/wit)
+set(bottom_half "${CMAKE_SOURCE_DIR}/libc-bottom-half")
+
+add_custom_target(
+  bindings
+  COMMAND
+    ${wit_bindgen} c
+      --autodrop-borrows yes
+      --rename-world wasip2
+      --type-section-suffix __wasi_libc
+      --world wasi:cli/imports@0.2.0
+      --rename wasi:clocks/monotonic-clock@0.2.0=monotonic_clock
+      --rename wasi:clocks/wall-clock@0.2.0=wall_clock
+      --rename wasi:filesystem/preopens@0.2.0=filesystem_preopens
+      --rename wasi:filesystem/types@0.2.0=filesystem
+      --rename wasi:io/error@0.2.0=io_error
+      --rename wasi:io/poll@0.2.0=poll
+      --rename wasi:io/streams@0.2.0=streams
+      --rename wasi:random/insecure-seed@0.2.0=random_insecure_seed
+      --rename wasi:random/insecure@0.2.0=random_insecure
+      --rename wasi:random/random@0.2.0=random
+      --rename wasi:sockets/instance-network@0.2.0=instance_network
+      --rename wasi:sockets/ip-name-lookup@0.2.0=ip_name_lookup
+      --rename wasi:sockets/network@0.2.0=network
+      --rename wasi:sockets/tcp-create-socket@0.2.0=tcp_create_socket
+      --rename wasi:sockets/tcp@0.2.0=tcp
+      --rename wasi:sockets/udp-create-socket@0.2.0=udp_create_socket
+      --rename wasi:sockets/udp@0.2.0=udp
+      --rename wasi:cli/environment@0.2.0=environment
+      --rename wasi:cli/exit@0.2.0=exit
+      --rename wasi:cli/stdin@0.2.0=stdin
+      --rename wasi:cli/stdout@0.2.0=stdout
+      --rename wasi:cli/stderr@0.2.0=stderr
+      --rename wasi:cli/terminal-input@0.2.0=terminal_input
+      --rename wasi:cli/terminal-output@0.2.0=terminal_output
+      --rename wasi:cli/terminal-stdin@0.2.0=terminal_stdin
+      --rename wasi:cli/terminal-stdout@0.2.0=terminal_stdout
+      --rename wasi:cli/terminal-stderr@0.2.0=terminal_stderr
+      ${wit_dir}
+  COMMAND cmake -E copy wasip2.h ${bottom_half}/headers/public/wasi/
+  COMMAND cmake -E copy wasip2_component_type.o ${bottom_half}/sources
+  COMMAND cmake -E copy wasip2.c ${bottom_half}/sources
+  COMMAND sed -i "'s_#include .wasip2\.h._#include \"wasi/wasip2.h\"_'" ${bottom_half}/sources/wasip2.c
+  COMMAND sed -i "s/extern void exit_exit/_Noreturn extern void exit_exit/" ${bottom_half}/headers/public/wasi/wasip2.h
+  COMMAND sed -i "s/extern void __wasm_import_exit_exit/_Noreturn extern void __wasm_import_exit_exit/" ${bottom_half}/sources/wasip2.c
+  DEPENDS wit-bindgen wasi-wits
+)

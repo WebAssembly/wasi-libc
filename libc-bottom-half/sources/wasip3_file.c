3d915cf237cdb1dae6922dde42c612b82764eb70
Christof Petig <christof.petig@arcor.de>
2026-01-02 21:35:55 +0100

    file stat

--- libc-bottom-half/sources/wasip3_file.c
+++ libc-bottom-half/sources/wasip3_file.c
@@ -3,6 +3,8 @@
 
 #ifdef __wasip3__
 #include <assert.h>
+#include <common/errors.h>
+#include <libc/sys/stat/stat_impl.h>
 #include <wasi/descriptor_table.h>
 
 typedef struct {
@@ -29,7 +31,50 @@ static int file_get_file(void *data, filesystem_borrow_descriptor_t *out_file) {
   return 0;
 }
 
-static int file_fstat(void *data, struct stat *buf) { abort(); }
+static void block_on(wasip3_subtask_status_t status) {
+  if (status != WASIP3_SUBTASK_RETURNED) {
+    if (WASIP3_SUBTASK_STATE(status) == WASIP3_SUBTASK_STARTED) {
+      wasip3_subtask_t handle = WASIP3_SUBTASK_HANDLE(status);
+      wasip3_waitable_set_t set = wasip3_waitable_set_new();
+      wasip3_waitable_join(handle, set);
+      wasip3_event_t event;
+      wasip3_waitable_set_wait(set, &event);
+      assert(event.event == WASIP3_EVENT_SUBTASK);
+      assert(event.code == WASIP3_SUBTASK_RETURNED);
+      wasip3_subtask_drop(event.waitable);
+      wasip3_waitable_set_drop(set);
+    } else {
+      abort();
+    }
+  }
+}
+
+static int file_fstat(void *data, struct stat *buf) {
+  file3_t *file = (file3_t *)data;
+  // Get the metadata hash for the file descriptor
+  filesystem_result_metadata_hash_value_error_code_t result;
+  wasip3_subtask_status_t status = filesystem_method_descriptor_metadata_hash(
+      filesystem_borrow_descriptor(file->file_handle), &result);
+  block_on(status);
+  if (result.is_err) {
+    translate_error(result.val.err);
+    return -1;
+  }
+
+  // Get the file metadata
+  filesystem_result_descriptor_stat_error_code_t stat_result;
+  status = filesystem_method_descriptor_stat(
+      filesystem_borrow_descriptor(file->file_handle), &stat_result);
+  block_on(status);
+  if (stat_result.is_err) {
+    translate_error(stat_result.val.err);
+    return -1;
+  }
+
+  // Convert the internal data to an external struct
+  to_public_stat(&result.val.ok, &stat_result.val.ok, buf);
+  return 0;
+}
 
 static int file_seek_end(file3_t *file) { abort(); }
 

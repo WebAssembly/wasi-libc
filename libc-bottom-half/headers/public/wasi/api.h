WebAssembly/wasi-libc

commit 8baf533cd89981615cac7bbf93a6eaf0e9e67ec7

Sever wasip2's dependence on wasip1 headers (#694)
This commit updates `wasi/api.h` to be fully undefined on WASIp2
targets. This prevents leaking WASIp1 definitions on unapplicable
targets, such as `wasm32-wasip2` and the upcoming `wasm32-wasip3`.

This involved changing a number of `#define`s from something symbolic to
a number literal. Static assertions throughout the codebase, already
present, assert that these numbers match WASIp1 ABI values and for
WASIp2+ these numbers are just a libc abstraction, not part of the ABI.

libc-bottom-half/headers/public/wasi/api.h
@@ -19,6 +19,8 @@
 #ifndef __wasi_api_h
 #define __wasi_api_h
 
+#ifndef __wasilibc_use_wasip2
+
 #ifndef __wasi__
 #error <wasi/api.h> is only supported on WASI platforms.
 #endif
@@ -1444,8 +1446,6 @@ typedef struct __wasi_prestat_t {
 
 _Static_assert(sizeof(__wasi_prestat_t) == 8, "witx calculated size");
 _Static_assert(_Alignof(__wasi_prestat_t) == 4, "witx calculated align");
-
-#ifndef __wasilibc_use_wasip2
 
 /**
  * @defgroup wasi_snapshot_preview1

libc-bottom-half/cloudlibc/src/common/clock.h
@@ -5,7 +5,8 @@
 #ifndef COMMON_CLOCK_H
 #define COMMON_CLOCK_H
 
-#include <wasi/api.h>
+#define CLOCKID_REALTIME 0
+#define CLOCKID_MONOTONIC 1
 
 // In this implementation we define clockid_t as a pointer type, so that
 // we can implement them as full objects. Right now we only use those
@@ -13,7 +14,7 @@
 // future we can use this to provide support for pthread_getcpuclockid()
 // and clock file descriptors.
 struct __clockid {
-  __wasi_clockid_t id;
+  int id;
 };
 
 #endif

libc-bottom-half/cloudlibc/src/common/errors.h
@@ -6,115 +6,115 @@
 static void translate_error(filesystem_error_code_t error) {
     switch (error) {
     case FILESYSTEM_ERROR_CODE_ACCESS:
-        errno = __WASI_ERRNO_ACCES;
+        errno = EACCES;
         break;
     case FILESYSTEM_ERROR_CODE_WOULD_BLOCK:
-        errno = __WASI_ERRNO_AGAIN;
+        errno = EAGAIN;
         break;
     case FILESYSTEM_ERROR_CODE_ALREADY:
-        errno = __WASI_ERRNO_ALREADY;
+        errno = EALREADY;
         break;
     case FILESYSTEM_ERROR_CODE_BAD_DESCRIPTOR:
-        errno = __WASI_ERRNO_BADF;
+        errno = EBADF;
         break;
     case FILESYSTEM_ERROR_CODE_BUSY:
-        errno = __WASI_ERRNO_BUSY;
+        errno = EBUSY;
         break;
     case FILESYSTEM_ERROR_CODE_DEADLOCK:
-        errno = __WASI_ERRNO_DEADLK;
+        errno = EDEADLK;
         break;
     case FILESYSTEM_ERROR_CODE_QUOTA:
-        errno = __WASI_ERRNO_DQUOT;
+        errno = EDQUOT;
         break;
     case FILESYSTEM_ERROR_CODE_EXIST:
-        errno = __WASI_ERRNO_EXIST;
+        errno = EEXIST;
         break;
     case FILESYSTEM_ERROR_CODE_FILE_TOO_LARGE:
-        errno = __WASI_ERRNO_FBIG;
+        errno = EFBIG;
         break;
     case FILESYSTEM_ERROR_CODE_ILLEGAL_BYTE_SEQUENCE:
-        errno = __WASI_ERRNO_ILSEQ;
+        errno = EILSEQ;
         break;
     case FILESYSTEM_ERROR_CODE_IN_PROGRESS:
-        errno = __WASI_ERRNO_INPROGRESS;
+        errno = EINPROGRESS;
         break;
     case FILESYSTEM_ERROR_CODE_INTERRUPTED:
-        errno = __WASI_ERRNO_INTR;
+        errno = EINTR;
         break;
     case FILESYSTEM_ERROR_CODE_INVALID:
-        errno = __WASI_ERRNO_INVAL;
+        errno = EINVAL;
         break;
     case FILESYSTEM_ERROR_CODE_IO:
-        errno = __WASI_ERRNO_IO;
+        errno = EIO;
         break;
     case FILESYSTEM_ERROR_CODE_IS_DIRECTORY:
-        errno = __WASI_ERRNO_ISDIR;
+        errno = EISDIR;
         break;
     case FILESYSTEM_ERROR_CODE_LOOP:
-        errno = __WASI_ERRNO_LOOP;
+        errno = ELOOP;
         break;
     case FILESYSTEM_ERROR_CODE_TOO_MANY_LINKS:
-        errno = __WASI_ERRNO_MLINK;
+        errno = EMLINK;
         break;
     case FILESYSTEM_ERROR_CODE_MESSAGE_SIZE:
-        errno = __WASI_ERRNO_MSGSIZE;
+        errno = EMSGSIZE;
         break;
     case FILESYSTEM_ERROR_CODE_NAME_TOO_LONG:
-        errno = __WASI_ERRNO_NAMETOOLONG;
+        errno = ENAMETOOLONG;
         break;
     case FILESYSTEM_ERROR_CODE_NO_DEVICE:
-        errno = __WASI_ERRNO_NODEV;
+        errno = ENODEV;
         break;
     case FILESYSTEM_ERROR_CODE_NO_ENTRY:
-        errno = __WASI_ERRNO_NOENT;
+        errno = ENOENT;
         break;
     case FILESYSTEM_ERROR_CODE_NO_LOCK:
-        errno = __WASI_ERRNO_NOLCK;
+        errno = ENOLCK;
         break;
     case FILESYSTEM_ERROR_CODE_INSUFFICIENT_MEMORY:
-        errno = __WASI_ERRNO_NOMEM;
+        errno = ENOMEM;
         break;
     case FILESYSTEM_ERROR_CODE_INSUFFICIENT_SPACE:
-        errno = __WASI_ERRNO_NOSPC;
+        errno = ENOSPC;
         break;
     case FILESYSTEM_ERROR_CODE_NOT_DIRECTORY:
-        errno = __WASI_ERRNO_NOTDIR;
+        errno = ENOTDIR;
         break;
     case FILESYSTEM_ERROR_CODE_NOT_EMPTY:
-        errno = __WASI_ERRNO_NOTEMPTY;
+        errno = ENOTEMPTY;
         break;
     case FILESYSTEM_ERROR_CODE_NOT_RECOVERABLE:
-        errno = __WASI_ERRNO_NOTRECOVERABLE;
+        errno = ENOTRECOVERABLE;
         break;
     case FILESYSTEM_ERROR_CODE_UNSUPPORTED:
-        errno = __WASI_ERRNO_NOTSUP;
+        errno = ENOTSUP;
         break;
     case FILESYSTEM_ERROR_CODE_NO_TTY:
-        errno = __WASI_ERRNO_NOTTY;
+        errno = ENOTTY;
         break;
     case FILESYSTEM_ERROR_CODE_NO_SUCH_DEVICE:
-        errno = __WASI_ERRNO_NXIO;
+        errno = ENXIO;
         break;
     case FILESYSTEM_ERROR_CODE_OVERFLOW:
-        errno = __WASI_ERRNO_OVERFLOW;
+        errno = EOVERFLOW;
         break;
     case FILESYSTEM_ERROR_CODE_NOT_PERMITTED:
-        errno = __WASI_ERRNO_PERM;
+        errno = EPERM;
         break;
     case FILESYSTEM_ERROR_CODE_PIPE:
-        errno = __WASI_ERRNO_PIPE;
+        errno = EPIPE;
         break;
     case FILESYSTEM_ERROR_CODE_READ_ONLY:
-        errno = __WASI_ERRNO_ROFS;
+        errno = EROFS;
         break;
     case FILESYSTEM_ERROR_CODE_INVALID_SEEK:
-        errno = __WASI_ERRNO_SPIPE;
+        errno = ESPIPE;
         break;
     case FILESYSTEM_ERROR_CODE_TEXT_FILE_BUSY:
-        errno = __WASI_ERRNO_TXTBSY;
+        errno = ETXTBSY;
         break;
     case FILESYSTEM_ERROR_CODE_CROSS_DEVICE:
-        errno = __WASI_ERRNO_XDEV;
+        errno = EXDEV;
         break;
     default:
         abort(); // Unreachable

libc-bottom-half/cloudlibc/src/libc/dirent/readdir.c
@@ -23,6 +23,7 @@
 
 #include "dirent_impl.h"
 
+#ifndef __wasilibc_use_wasip2
 static_assert(DT_BLK == __WASI_FILETYPE_BLOCK_DEVICE, "Value mismatch");
 static_assert(DT_CHR == __WASI_FILETYPE_CHARACTER_DEVICE, "Value mismatch");
 static_assert(DT_DIR == __WASI_FILETYPE_DIRECTORY, "Value mismatch");
@@ -30,6 +31,7 @@ static_assert(DT_FIFO == __WASI_FILETYPE_SOCKET_STREAM, "Value mismatch");
 static_assert(DT_LNK == __WASI_FILETYPE_SYMBOLIC_LINK, "Value mismatch");
 static_assert(DT_REG == __WASI_FILETYPE_REGULAR_FILE, "Value mismatch");
 static_assert(DT_UNKNOWN == __WASI_FILETYPE_UNKNOWN, "Value mismatch");
+#endif
 
 // Grows a buffer to be large enough to hold a certain amount of data.
 #define GROW(buffer, buffer_size, target_size)      \

libc-bottom-half/cloudlibc/src/libc/errno/errno.c
@@ -7,6 +7,7 @@
 #include <errno.h>
 #include <threads.h>
 
+#ifndef __wasilibc_use_wasip2
 static_assert(E2BIG == __WASI_ERRNO_2BIG, "Value mismatch");
 static_assert(EACCES == __WASI_ERRNO_ACCES, "Value mismatch");
 static_assert(EADDRINUSE == __WASI_ERRNO_ADDRINUSE, "Value mismatch");
@@ -83,5 +84,6 @@ static_assert(ESTALE == __WASI_ERRNO_STALE, "Value mismatch");
 static_assert(ETIMEDOUT == __WASI_ERRNO_TIMEDOUT, "Value mismatch");
 static_assert(ETXTBSY == __WASI_ERRNO_TXTBSY, "Value mismatch");
 static_assert(EXDEV == __WASI_ERRNO_XDEV, "Value mismatch");
+#endif
 
 thread_local int errno = 0;

libc-bottom-half/cloudlibc/src/libc/fcntl/openat.c
@@ -17,6 +17,7 @@
 #include <fcntl.h>
 #include <string.h>
 
+#ifndef __wasilibc_use_wasip2
 static_assert(O_APPEND == __WASI_FDFLAGS_APPEND, "Value mismatch");
 static_assert(O_DSYNC == __WASI_FDFLAGS_DSYNC, "Value mismatch");
 static_assert(O_NONBLOCK == __WASI_FDFLAGS_NONBLOCK, "Value mismatch");
@@ -27,6 +28,7 @@ static_assert(O_CREAT >> 12 == __WASI_OFLAGS_CREAT, "Value mismatch");
 static_assert(O_DIRECTORY >> 12 == __WASI_OFLAGS_DIRECTORY, "Value mismatch");
 static_assert(O_EXCL >> 12 == __WASI_OFLAGS_EXCL, "Value mismatch");
 static_assert(O_TRUNC >> 12 == __WASI_OFLAGS_TRUNC, "Value mismatch");
+#endif
 
 int __wasilibc_nocwd_openat_nomode(int fd, const char *path, int oflag) {
 

libc-bottom-half/cloudlibc/src/libc/fcntl/posix_fadvise.c
@@ -13,6 +13,7 @@
 #include <errno.h>
 #include <fcntl.h>
 
+#ifndef __wasilibc_use_wasip2
 static_assert(POSIX_FADV_DONTNEED == __WASI_ADVICE_DONTNEED,
               "Value mismatch");
 static_assert(POSIX_FADV_NOREUSE == __WASI_ADVICE_NOREUSE, "Value mismatch");
@@ -22,6 +23,7 @@ static_assert(POSIX_FADV_SEQUENTIAL == __WASI_ADVICE_SEQUENTIAL,
               "Value mismatch");
 static_assert(POSIX_FADV_WILLNEED == __WASI_ADVICE_WILLNEED,
               "Value mismatch");
+#endif
 
 int posix_fadvise(int fd, off_t offset, off_t len, int advice) {
   if (offset < 0 || len < 0)

libc-bottom-half/cloudlibc/src/libc/sys/stat/fstatat.c
@@ -46,9 +46,9 @@ int __wasilibc_nocwd_fstatat(int fd, const char *restrict path, struct stat *res
   }
 
   // Create lookup properties.
-  __wasi_lookupflags_t lookup_flags = 0;
+  filesystem_path_flags_t lookup_flags = 0;
   if ((flag & AT_SYMLINK_NOFOLLOW) == 0)
-    lookup_flags |= __WASI_LOOKUPFLAGS_SYMLINK_FOLLOW;
+    lookup_flags |= FILESYSTEM_PATH_FLAGS_SYMLINK_FOLLOW;
 
   // Perform system call.
   filesystem_descriptor_stat_t internal_stat;

commit f440981235ee4872d0b758bb98e56903321f3388

Fix a C++-specific bug in #694
This fixes the order `#define`s showed up in for C++ introduced in #694
to fix the inclusion of this repository into wasi-sdk. I don't know of a
great way to test this without building a whole C++ sysroot which seems
out-of-scope, so this is something I think will best be left for "try to
not break it and we'll catch it in wasi-sdk".

libc-bottom-half/headers/public/wasi/api.h
@@ -2119,10 +2119,10 @@ int32_t __wasi_thread_spawn(
     void *start_arg) __attribute__((__warn_unused_result__));
 #endif
 
-#endif // __wasilibc_use_wasip2
-
 #ifdef __cplusplus
 }
 #endif
+
+#endif // __wasilibc_use_wasip2
 
 #endif
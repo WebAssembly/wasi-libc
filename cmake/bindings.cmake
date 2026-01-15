8e553aca284609a63be304253bb6213b523ecf6c
Sy Brand <sy.brand@fastly.com>
2026-01-15 17:04:13 +0000

    Correct sed on mac

--- cmake/bindings.cmake
+++ cmake/bindings.cmake
@@ -168,12 +168,18 @@ add_custom_target(
 
 add_custom_target(bindings DEPENDS bindings-p2 bindings-p3)
 
+if(CMAKE_SYSTEM_NAME STREQUAL "Darwin")
+  set(SED_INPLACE_FLAG "''")
+else()
+  set(SED_INPLACE_FLAG "")
+endif()
+
 function(wit_bindgen_edit p)
   add_custom_target(
     bindings-${p}-edit
-    COMMAND sed -i '' "'s_#include .wasi${p}\.h._#include \"wasi/wasi${p}.h\"_'" ${bottom_half}/sources/wasi${p}.c
-    COMMAND sed -i '' "s/extern void exit_exit/_Noreturn extern void exit_exit/" ${bottom_half}/headers/public/wasi/__generated_wasi${p}.h
-    COMMAND sed -i '' "s/extern void __wasm_import_exit_exit/_Noreturn extern void __wasm_import_exit_exit/" ${bottom_half}/sources/wasi${p}.c
+    COMMAND sed -i ${SED_INPLACE_FLAG} "'s_#include .wasi${p}\.h._#include \"wasi/wasi${p}.h\"_'" ${bottom_half}/sources/wasi${p}.c
+    COMMAND sed -i ${SED_INPLACE_FLAG} "s/extern void exit_exit/_Noreturn extern void exit_exit/" ${bottom_half}/headers/public/wasi/__generated_wasi${p}.h
+    COMMAND sed -i ${SED_INPLACE_FLAG} "s/extern void __wasm_import_exit_exit/_Noreturn extern void __wasm_import_exit_exit/" ${bottom_half}/sources/wasi${p}.c
     DEPENDS bindings-${p}
   )
   add_dependencies(bindings bindings-${p}-edit)

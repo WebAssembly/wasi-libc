23aad339e8a85212d2f684be3ecc61d146118e34
Alex Crichton <alex@alexcrichton.com>
2026-02-13 07:50:35 -0800

    Adjust tests for sleeping
    Assert a lower bound for sleeping, not an upper bound, to handle the
    case that the system is busy so the sleep might take longer than
    intended.

--- test/src/nanosleep.c
+++ test/src/nanosleep.c
@@ -27,13 +27,7 @@ int main(void) {
 
   long nanoseconds_elapsed = (end_time.tv_sec - start_time.tv_sec) * 1E9 -
                              start_time.tv_nsec + end_time.tv_nsec;
-
-  // Test that the difference between the requested amount of sleep
-  // and the actual elapsed time is within an acceptable margin
-  double difference = abs(nanoseconds_elapsed - ns_to_sleep) / ns_to_sleep;
-
-  // Allow the actual sleep time to be twice as much as the requested time
-  TEST(difference <= 1);
+  TEST(nanoseconds_elapsed >= ns_to_sleep);
 
   return t_status;
 }

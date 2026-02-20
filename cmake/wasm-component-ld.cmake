28d741f30215a7d0da4e4a2812c3c5bb774e30b5
Sy Brand <sy.brand@fastly.com>
2026-02-20 22:41:52 +0000

    Bump wasm-component-ld

--- cmake/wasm-component-ld.cmake
+++ cmake/wasm-component-ld.cmake
@@ -10,7 +10,7 @@ if (NOT WASM_COMPONENT_LD_EXECUTABLE)
   ba_download(
     wasm-component-ld
     "https://github.com/bytecodealliance/wasm-component-ld"
-    "v0.5.20"
+    "v0.5.21"
   )
   ExternalProject_Get_Property(wasm-component-ld SOURCE_DIR)
   set(WASM_COMPONENT_LD_EXECUTABLE "${SOURCE_DIR}/wasm-component-ld")

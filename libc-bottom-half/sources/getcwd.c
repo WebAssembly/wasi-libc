94dd50313ab9efa464d10872122df78b1f528ed8
Sy Brand <sy.brand@fastly.com>
2026-02-20 20:27:32 +0000

    Change cwd locking

--- libc-bottom-half/sources/getcwd.c
+++ libc-bottom-half/sources/getcwd.c
@@ -8,27 +8,32 @@ char *__wasilibc_cwd = "/";
 
 #ifdef _REENTRANT
 static __lock_t lock[1];
+// Critical section contains no yield points, so we can use weak locks.
+void __wasilibc_cwd_lock(void) { WEAK_LOCK(lock); }
+void __wasilibc_cwd_unlock(void) { WEAK_UNLOCK(lock); }
+#else
+#define __wasilibc_cwd_lock() (void)0
+#define __wasilibc_cwd_unlock() (void)0
 #endif
 
 char *getcwd(char *buf, size_t size) {
-  // Critical section contains no yield points, so we can use weak locks.
-  WEAK_LOCK(lock);
+  __wasilibc_cwd_lock();
   if (!buf) {
     buf = strdup(__wasilibc_cwd);
     if (!buf) {
       errno = ENOMEM;
-      WEAK_UNLOCK(lock);
+      __wasilibc_cwd_unlock();
       return NULL;
     }
   } else {
     size_t len = strlen(__wasilibc_cwd);
     if (size < len + 1) {
       errno = ERANGE;
-      WEAK_UNLOCK(lock);
+      __wasilibc_cwd_unlock();
       return NULL;
     }
     strcpy(buf, __wasilibc_cwd);
   }
-  WEAK_UNLOCK(lock);
+  __wasilibc_cwd_unlock();
   return buf;
 }

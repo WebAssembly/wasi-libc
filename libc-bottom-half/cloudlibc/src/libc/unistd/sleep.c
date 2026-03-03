bc269ebb0db3c4ced09eff59bd02be7e5256b141
Sy Brand <sy.brand@fastly.com>
2026-03-03 15:25:32 +0000

    Use monotonic clock for `sleep` (#753)
    Currently, `sleep` calls `clock_nanosleep` with `CLOCK_REALTIME`, which
    unconditionally returns `ENOTSUP` on WASIp{2,3}. This PR changes this to
    use `CLOCK_MONOTONIC`.

--- libc-bottom-half/cloudlibc/src/libc/unistd/sleep.c
+++ libc-bottom-half/cloudlibc/src/libc/unistd/sleep.c
@@ -4,10 +4,20 @@
 
 #include <time.h>
 #include <unistd.h>
+#include <wasi/version.h>
 
 unsigned int sleep(unsigned int seconds) {
   struct timespec ts = {.tv_sec = seconds, .tv_nsec = 0};
-  if (clock_nanosleep(CLOCK_REALTIME, 0, &ts, NULL) != 0)
+  // FIXME(WebAssembly/WASI#857): wasip2/p3 only supports the monotonic clock for
+  // sleeping.
+  #if defined(__wasip1__)
+  clockid_t clock_id = CLOCK_REALTIME;
+  #elif defined(__wasip2__) || defined(__wasip3__)
+  clockid_t clock_id = CLOCK_MONOTONIC;
+  #else
+  #error "Unsupported WASI version"
+  #endif
+  if (clock_nanosleep(clock_id, 0, &ts, NULL) != 0)
     return seconds;
   return 0;
 }

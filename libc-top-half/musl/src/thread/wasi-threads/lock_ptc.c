1000351ec2af1c4ca07653e943246f795ee28d38
Sy Brand <sy.brand@fastly.com>
2026-03-12 09:24:08 +0000

    Move lock_ptc

--- libc-top-half/musl/src/thread/wasi-threads/lock_ptc.c
+++ libc-top-half/musl/src/thread/wasi-threads/lock_ptc.c
@@ -1,6 +1,5 @@
 #include <pthread.h>
 
-#if defined(_REENTRANT) && !defined(__wasi_cooperative_threads__)
 static pthread_rwlock_t lock = PTHREAD_RWLOCK_INITIALIZER;
 
 void __inhibit_ptc()
@@ -17,8 +16,3 @@ void __release_ptc()
 {
 	pthread_rwlock_unlock(&lock);
 }
-#else
-void __inhibit_ptc(){}
-void __acquire_ptc(){}
-void __release_ptc(){}
-#endif

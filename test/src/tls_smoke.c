7c1c53d8837d492f767f3fa11684594d86021cbd
Alex Crichton <alex@alexcrichton.com>
2026-07-08 16:15:32 -0500

    Add a smoke test for static TLS (#818)
    I was editing some stuff around this and my edits were wrong yet tests
    passed. Add a test to ensure this doesn't regress.

---
+++ test/src/tls_smoke.c
@@ -0,0 +1,32 @@
+#include "test.h"
+#include <pthread.h>
+
+#define TEST(c)                                                                \
+  do {                                                                         \
+    if (!(c))                                                                  \
+      t_error("%s failed\n", #c);                                              \
+  } while (0)
+
+static __thread int static_tls = 3;
+
+static void *worker(void *arg) {
+  (void)arg;
+
+  TEST(static_tls == 3);
+  static_tls = 5;
+
+  return NULL;
+}
+
+int main(void) {
+  TEST(static_tls == 3);
+  static_tls = 4;
+  pthread_t t;
+  if (pthread_create(&t, NULL, worker, NULL) == 0) {
+    TEST(static_tls == 4);
+    TEST(pthread_join(t, NULL) == 0);
+  }
+  TEST(static_tls == 4);
+
+  return t_status;
+}

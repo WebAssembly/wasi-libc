WebAssembly/wasi-libc

commit 7d831af67cc41327a826b446e90eff8fbe4fb601

Adjust how TLS initialization works (#820)
In reviewing various issues and TLS initialization I was personally left
pretty confused. There were more hooks into TLS than I had anticipated
and I found them to interact in confusing ways. Notably `__copy_tls`
initializes another thread's TLS pointer but uses the `__wasm_init_tls`
intrinsic which sets the current thread's TLS pointer as well. This
means that there was previously a dance that was done to save/restore
this as necessary.

I additionally noticed that TLS initialization for WASIp3 is expected to
be a bit more expensive than other platforms due to an intrinsic
necessary to learn the current TID. This is something that I think would
be worthwhile to take off the hot path.

Finally, I felt that the assembly as part of task initialization was
doing a bit too much relative to what could be done in C.

Adding all these together, the changes here are:

* For wasip3 coop threads, sync tasks use the "main thread" TLS setup in
the `start` function. They no longer allocate their own block of TLS
storage.
* For wasip3 coop threads, async tasks now delegate to C to perform
stack allocation. TLS is then stored at the top of the stack, and the
stack top is returned to assembly to get configured.
* The `__copy_tls` function is no longer used on wasip3 targets with
coop threads. Task initialization after the changes above no longer used
it, and spawned threads now initialize their TLS when they start.
* The `__init_tp` function is now deferred as an internal detail of
`__pthread_self`. This is effectively lazy initialization of
`__pthread_self()`. The CRT startup objects thus no longer need this
symbol for example.
* When spawning a thread, assembly only initializes the stack pointer
and lets C initialize TLS.
* A workaround to consider `__wasi_coop_thread_start` used is removed
since I believe it's not necessary as the symbol is referred to by
function pointer to pass to an imported function.

libc-top-half/musl/src/internal/pthread_impl.h
@@ -129,12 +129,20 @@ enum {
 #define DTP_OFFSET 0
 #endif
 
+hidden int __init_tp(void *);
+
 #ifdef TLS_ABOVE_TP
 #define TP_ADJ(p) ((char *)(p) + sizeof(struct pthread) + TP_OFFSET)
 #define __pthread_self() ((pthread_t)(__get_tp() - sizeof(struct __pthread) - TP_OFFSET))
+#error "wasi-libc doesn't support this setting"
 #else
 #define TP_ADJ(p) (p)
-#define __pthread_self() ((pthread_t)__get_tp())
+static inline pthread_t __pthread_self() {
+  pthread_t ret = (pthread_t) __get_tp();
+  if (ret->tid == 0)
+    __init_tp(ret);
+  return ret;
+}
 #endif
 
 #ifndef tls_mod_off_t
@@ -154,8 +162,9 @@ enum {
 	 0x80000000 })
 
 void *__tls_get_addr(tls_mod_off_t *);
-hidden int __init_tp(void *);
+#if defined(_REENTRANT) && !defined(__wasi_cooperative_threads__)
 hidden void *__copy_tls(unsigned char *);
+#endif
 hidden void __reset_tls();
 
 hidden void __membarrier_init(void);

libc-bottom-half/crt/crt1-command.c
@@ -4,7 +4,6 @@
 
 #include <wasi/api.h>
 
-extern void __wasi_init_tp(void);
 extern void __wasm_call_ctors(void);
 extern int __main_void(void);
 extern void __wasm_call_dtors(void);
@@ -51,8 +50,6 @@ __attribute__((export_name("wasi:cli/run@0.3.0#run"))) int _start(void)
   }
   started = 1;
 #endif
-
-  __wasi_init_tp();
 
   // The linker synthesizes this to call constructors.
   __wasm_call_ctors();

libc-bottom-half/crt/crt1-reactor.c
@@ -1,7 +1,6 @@
 #if defined(_REENTRANT)
 #include <stdatomic.h>
 #endif
-extern void __wasi_init_tp(void);
 extern void __wasm_call_ctors(void);
 
 #ifdef __wasm_libcall_thread_context__
@@ -27,7 +26,6 @@ __attribute__((export_name("_initialize"))) void _initialize(void) {
   }
   initialized = 1;
 #endif
-  __wasi_init_tp();
 
   // The linker synthesizes this to call constructors.
   __wasm_call_ctors();

libc-bottom-half/sources/__wasm_init_task.S
@@ -4,16 +4,13 @@
 
   .globaltype  __init_stack_pointer, i32, immutable
   .functype   __wasm_set_stack_pointer (i32) -> ()
+  .globl __wasm_set_stack_pointer
 
 #ifdef __wasi_cooperative_threads__
   .globaltype __init_tls_base, i32, immutable
   .functype   __wasm_set_tls_base (i32) -> ()
-  .globaltype __tls_size, i32, immutable
-  .globaltype __tls_align, i32, immutable
-  .functype    malloc (i32) -> (i32)
-  .functype   __copy_tls (i32) -> (i32)
-  .functype   __allocate_tls () -> (i32)
-  .functype  __wasi_init_tp() -> ()
+  .functype   __wasilibc_init_async_task () -> (i32)
+  .functype   __wasi_init_tp () -> ()
 #endif
 
   .globl  __wasm_init_task
@@ -27,20 +24,26 @@
 __wasm_init_task:
   .functype  __wasm_init_task () -> ()
 
+  // Synchronous tasks will return before anything else can enter this
+  // component, so use the initial stack for this task.
   global.get   __init_stack_pointer
   call __wasm_set_stack_pointer
 
 #ifdef __wasi_cooperative_threads__
-
+  // For synchronous tasks in coop threads they inherit the same TLS as the
+  // original "main thread" that started the module.
+  //
+  // FIXME(#819) - the `__init_tls_base` global isn't correct right now, so
+  // use the `__stack_high` symbol as a kludge workaround for test while things
+  // are broken. Note that `__stack_high` isn't defined in PIC mode though, but
+  // coop threads are currently otherwise broken with PIC (#813) so there's
+  // other work to do there.
+#ifdef __PIC__
   global.get   __init_tls_base
+#else
+  i32.const __stack_high
+#endif
   call __wasm_set_tls_base
-
-  # Allocate a new TLS area
-  call __allocate_tls
-  call __copy_tls
-  call __wasm_set_tls_base
-
-  call __wasi_init_tp
 #endif
 
   end_function
@@ -48,31 +51,24 @@ __wasm_init_task:
 __wasm_init_async_task:
   .functype  __wasm_init_async_task () -> ()
 
-#ifdef __wasi_cooperative_threads__
-  # malloc and __init_tls may use the stack pointer and TLS base, so set those first
-  # to the statically-allocated values used for synchronous tasks.
+  // Setup a temporary stack for this task to invoke `malloc` on. Note that if
+  // threading is disabled then this is the actual stack of this task since no
+  // other tasks can enter while this is blocked and/or actively running.
   global.get   __init_stack_pointer
   call __wasm_set_stack_pointer
 
+#ifdef __wasi_cooperative_threads__
+  // Setup the initial TLS from the "main thread" in case `malloc` uses TLS.
+  // This won't be the TLS used by this task, however, in the long run.
   global.get   __init_tls_base
   call __wasm_set_tls_base
 
-  # Allocate a new stack
-  # Constant copied from __default_stacksize, TODO(wasip3) find a way
-  # to reference this constant without linking failing for shared libraries,
-  # i.e. a position-independent data reference.
-  i32.const 131072
-  call malloc
+  // Defer to the C code in wasi-libc to finish initialization. This'll
+  // allocate a new stack, setup this thread's TLS block, etc. The return
+  // value of the function is what to use a the stack pointer for the rest of
+  // this async task.
+  call __wasilibc_init_async_task
   call __wasm_set_stack_pointer
-
-  # Allocate a new TLS area
-  call __allocate_tls
-  call __copy_tls
-  call __wasm_set_tls_base
-
-  call __wasi_init_tp
-#else
-  call __wasm_init_task
 #endif
 
   end_function

libc-top-half/musl/src/env/__init_tls.c
@@ -8,6 +8,7 @@
 #endif
 #include <string.h>
 #include <stddef.h>
+#include <assert.h>
 #include "pthread_impl.h"
 #include "libc.h"
 #include "atomic.h"
@@ -86,9 +87,6 @@ static inline struct stack_bounds get_stack_bounds()
 	return bounds;
 }
 
-void __wasi_init_tp() {
-	__init_tp((void *)__get_tp());
-}
 #endif
 
 int __init_tp(void *p)
@@ -156,7 +154,7 @@ static struct tls_module main_tls;
 extern void __wasm_init_tls(void*);
 #endif
 
-#if defined(__wasilibc_unmodified_upstream) || defined(_REENTRANT)
+#if defined(_REENTRANT) && !defined(__wasi_cooperative_threads__)
 void *__copy_tls(unsigned char *mem)
 {
 #ifdef __wasilibc_unmodified_upstream
@@ -197,14 +195,9 @@ void *__copy_tls(unsigned char *mem)
 	mem += tls_align;
 	mem -= (uintptr_t)mem & (tls_align-1);
 	__wasm_init_tls(mem);
-	#ifdef __wasi_cooperative_threads__
-	void __wasm_set_tls_base(volatile void *ptr);
-	__wasm_set_tls_base(tls_base);
-	#else
   	__asm__("local.get %0\n"
 			"global.set __tls_base\n"
 			:: "r"(tls_base));
-	#endif
 	return mem;
 #endif
 }
@@ -293,11 +286,41 @@ static void static_init_tls(size_t *aux)
 weak_alias(static_init_tls, __init_tls);
 #endif
 
-#ifdef _REENTRANT
-void* __allocate_tls() {
-	void* mem;
-	if (posix_memalign(&mem, __builtin_wasm_tls_align(), __builtin_wasm_tls_size()) != 0)
-          __builtin_trap();
-	return mem;
+#ifdef __wasi_cooperative_threads__
+// Entrypoint for all new async tasks, invoked from `__wasm_init_async_task`.
+//
+// This is responsible for allocating a new stack for this async task in
+// addition to initializing TLS. TLS right now is stored at the top of the
+// stack.
+hidden void* __wasilibc_init_async_task(void) {
+  // Attempt to use the same stack size as the LLD-initialized stack (as
+  // reported by `get_stack_bounds`). If that failed (e.g. in PIC) mode then
+  // choose a best-effort constant.
+  size_t stack_size = get_stack_bounds().size;
+  if (stack_size == 0)
+    stack_size = __default_stacksize;
+
+  // Allocate the stack, trapping if allocation fails.
+  //
+  // FIXME(#810) this is never deallocated.
+  void* task_stack = malloc(stack_size);
+  if (task_stack == NULL)
+    __builtin_trap();
+
+  // TLS is stored at the top of the stack, and its initial address is
+  // aligned-down based on its requirement. Note that `__wasm_init_tls` serves
+  // double-duty of initializing using `memory.init` to initialize TLS while
+  // additionally setting the TLS base for this task.
+  uintptr_t stack_top = (uintptr_t)task_stack + stack_size;
+  uintptr_t tls_base_unaligned = stack_top - __builtin_wasm_tls_size();
+  uintptr_t tls_base = tls_base_unaligned & -__builtin_wasm_tls_align();
+  assert(tls_base >= (uintptr_t) task_stack);
+  __wasm_init_tls((void*)tls_base);
+
+  // The stack itself is always 16-byte aligned, so align down as necessary.
+  // Return this so the assembly shim can set this as the current stack pointer.
+  uintptr_t stack_init_aligned = tls_base & -16;
+  assert(stack_init_aligned >= (uintptr_t) task_stack);
+  return (void*) stack_init_aligned;
 }
 #endif

libc-top-half/musl/src/thread/coop-threads/__wasi_coop_thread_start.s
@@ -1,29 +1,25 @@
-	.text
+  .text
 
-	.export_name	__wasi_coop_thread_start, __wasi_coop_thread_start
+  .export_name  __wasi_coop_thread_start, __wasi_coop_thread_start
 
-	.globaltype	__init_stack_pointer, i32
-	.functype	__wasi_coop_thread_start_C (i32) -> ()
-	.functype   __wasm_set_tls_base (i32) -> ()
-    .functype   __wasm_set_stack_pointer (i32) -> ()
+  .functype   __wasi_coop_thread_start_C (i32) -> ()
+  .functype   __wasm_init_tls (i32) -> ()
+  .functype   __wasm_set_stack_pointer (i32) -> ()
 
-	.hidden	__wasi_coop_thread_start
-	.globl	__wasi_coop_thread_start
-	.type	__wasi_coop_thread_start,@function
+  .hidden  __wasi_coop_thread_start
+  .globl  __wasi_coop_thread_start
+  .type  __wasi_coop_thread_start,@function
 
 __wasi_coop_thread_start:
-	.functype	__wasi_coop_thread_start (i32) -> ()
+  .functype  __wasi_coop_thread_start (i32) -> ()
 
-	# Set up the minimum C environment.
-	local.get   0  # start_arg
-	i32.load    0  # stack
-	call __wasm_set_stack_pointer
+  # Configure the linear memory stack, as set up by the spawning thread.
+  local.get   0  # start_arg
+  i32.load    0  # stack
+  call __wasm_set_stack_pointer
 
-	local.get   0  # start_arg
-	i32.load    4  # tls_base
-	call __wasm_set_tls_base
-	# Make the C function do the rest of work.
-	local.get   0  # start_arg
-	call __wasi_coop_thread_start_C
+  # Now w
+  local.get   0  # start_arg
+  call __wasi_coop_thread_start_C
 
-	end_function
+  end_function

libc-top-half/musl/src/thread/coop-threads/pthread_create.c
@@ -23,6 +23,14 @@ static void process_map_base_deferred_free() {
   map_base_deferred_free = NULL;
 }
 
+// Defined in `__wasi_coop_thread_start.s` and not actually callable from C,
+// this is the component-model entrypoint.
+extern void __wasi_coop_thread_start(void *context);
+
+// Synthesized by wasm-ld this is called at the start of each new thread to
+// initialize its own TLS block.
+extern void __wasm_init_tls(void *ptr);
+
 // There is currently no thread.exit intrinsic, so pthread_exit
 // cannot terminate a thread early. As such, we do not expose it to users.
 // TODO(wasip3) revisit this if we add a thread.exit intrinsic
@@ -110,33 +118,45 @@ void __do_cleanup_pop(struct __ptcb *cb) {
 }
 
 struct start_args {
-  /*
-   * Note: the offset of the "stack" and "tls_base" members
-   * in this structure is hardcoded in wasip3_thread_start.
-   */
+  // Note that this `stack` member must be first as its offset is referenced
+  // from `__wasi_coop_thread_start` within the `__wasi_coop_thread_start.s`
+  // file.
+  //
+  // Otherwise though this is the initial stack pointer of the thread-to-be.
   void *stack;
+
+  // The TLS base pointer for this new thread. Not yet initialized, but
+  // allocated and has appropriate alignment.
   void *tls_base;
+
+  // A pointer to where this thread's `struct pthread` block is located. This is
+  // within `tls_base` and is used during initialization.
+  pthread_t self;
+
+  // The arguments used to execute this thread.
   void *(*start_func)(void *);
   void *start_arg;
 };
 
-/*
- * We want to ensure __wasi_coop_thread_start is linked whenever
- * pthread_create is used. The following reference is to ensure that.
- * Otherwise, the linker doesn't notice the dependency because
- * __wasi_coop_thread_start is used indirectly via a wasm export.
- */
-void __wasi_coop_thread_start(void *context);
-hidden void *__dummy_reference = __wasi_coop_thread_start;
+hidden void __wasi_coop_thread_start_C(struct start_args *args) {
+  // First thing to do on this new thread is initialize TLS. At the start of a
+  // new thread our tls base is 0, so it needs to be set to `args->tls_base`.
+  // TLS initialization happens through the `wasm-ld` provided symbol of
+  // `__wasm_init_tls` which will `memory.init` this pointer and additionally
+  // store the pointer into our tls base slot.
+  //
+  // Note that this thread's own `struct pthread`, however, lives within the TLS
+  // block and is already initialized. We don't want the default initialization
+  // here as well. The contents of our pthread block are thus saved/restored
+  // around the `__wasm_init_tls` call.
+  assert(((uintptr_t)args->tls_base) % __builtin_wasm_tls_align() == 0);
+  struct pthread self_copy = *args->self;
+  __wasm_init_tls(args->tls_base);
+  *__pthread_self() = self_copy;
 
-hidden void __wasi_coop_thread_start_C(void *context) {
-#ifdef __wasip3__
-  int tid = wasip3_thread_index();
-#else
-#error "Unknown WASI version"
-#endif
-  struct start_args *args = context;
-  __pthread_self()->tid = tid;
+  // Our tid should be configured in `pthread_create`, but double check it.
+  assert(__pthread_self()->tid == wasip3_thread_index());
+
   __pthread_exit(args->start_func(args->start_arg));
 }
 
@@ -233,7 +253,8 @@ int __pthread_create(pthread_t *restrict res,
     }
   }
 
-  new_tls_base = __copy_tls(tsd - tls_size);
+  void *unaligned_tls_base = tsd - tls_size;
+  new_tls_base = (void *)(((uintptr_t)unaligned_tls_base) & -tls_align);
 
   /* Compute pthread struct offset from old TLS base, apply to new TLS base */
   tls_offset = (uintptr_t)self - (uintptr_t)tls_base;
@@ -272,6 +293,7 @@ int __pthread_create(pthread_t *restrict res,
   args->start_func = entry;
   args->start_arg = arg;
   args->tls_base = (void *)new_tls_base;
+  args->self = new;
 
   if (!libc.threads_minus_1++)
     libc.need_locks = 1;

commit c89d5964422c9c58300ad37d10eb239eb0505426

Fix recursive thread initialization on the main thread
This commit fixes issues running upstream Python tests with threads that
was introduced in #820 as an accidental regression. Specifically the
condition used for lazy initialization, the TID is 0, is also true
during thread teardown when the TID is cleared. The condition for "lazy
init needed" is switched to handle something more robust, the
self-pointer in the TLS block.

libc-top-half/musl/src/internal/pthread_impl.h
@@ -138,7 +138,13 @@ hidden int __init_tp(void *);
 #define TP_ADJ(p) (p)
 static inline pthread_t __pthread_self() {
   pthread_t ret = (pthread_t) __get_tp();
-  if (ret->tid == 0)
+  // All user-spawned threads will initialize their TLS block at the start of
+  // the thread itself. For the main thread though the initialization is
+  // deferred until pthread functions are actually used to defer acquiring the
+  // current thread's ID. This is detected here by if the main thread's
+  // self-pointer hasn't yet been initialized then it's time to init the data
+  // for the main thread.
+  if (ret->self != ret)
     __init_tp(ret);
   return ret;
 }

test/CMakeLists.txt
@@ -512,6 +512,7 @@ if (ENABLE_COOP_THREADS)
   add_wasilibc_test(coop_sem_multiwaiter.c)
   add_wasilibc_test(coop_cond_signal_wakeup.c)
   add_wasilibc_test(coop_stdio_orphan_lock.c)
+  add_wasilibc_test(coop_key_delete_after_exit.c)
   add_wasilibc_test(coop_barrier_reuse.c)
   add_wasilibc_test(coop_join_uaf.c)
   add_wasilibc_test(coop_timedwaits.c)

test/src/coop_key_delete_after_exit.c
@@ -1,0 +1,31 @@
+#include "test.h"
+#include <pthread.h>
+#include <stdio.h>
+
+#define TEST(c)                                                                \
+  do {                                                                         \
+    if (!(c))                                                                  \
+      t_error("%s failed\n", #c);                                              \
+  } while (0)
+
+static void *worker(void *arg) {
+  (void)arg;
+  return NULL;
+}
+
+int main(void) {
+  // Pull in extra management of locking w.r.t. threads.
+  flockfile(stdout);
+  funlockfile(stdout);
+
+  pthread_key_t key;
+  TEST(pthread_key_create(&key, NULL) == 0);
+
+  pthread_t t;
+  TEST(pthread_create(&t, NULL, worker, NULL) == 0);
+  TEST(pthread_join(t, NULL) == 0);
+
+  TEST(pthread_key_delete(key) == 0);
+
+  return t_status;
+}
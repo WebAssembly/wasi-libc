65d4a7be5e21af1bc7a5a4cfa18403ab446547a5
Sy Brand <sy.brand@fastly.com>
2026-07-20 13:04:37 +0100

    Change single threaded impl

--- libc-top-half/musl/src/thread/single-threaded/__wait.c
+++ libc-top-half/musl/src/thread/single-threaded/__wait.c
@@ -1,12 +1,18 @@
-// We cannot support futexes in single-threaded mode, so we just trap if any code tries to use them.
+// We cannot support futexes in single-threaded mode, so we just trap if we need to wait, and make waking a no-op.
 #include <wasi/libc.h>
 int __wasilibc_futex_wait(volatile int *addr, int val, clockid_t clk,
                           const struct timespec *at, unsigned flags)
 {
+  if (*addr != val) {
+    return -EWOULDBLOCK;
+  }
   __builtin_trap();
 }
 
 void __wasilibc_futex_wake(volatile int *addr, int count, unsigned flags)
 {
-  __builtin_trap();
+  (void)addr;
+  (void)count;
+  (void)flags;
+  // no-op
 }

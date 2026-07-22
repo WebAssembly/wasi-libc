323e68664d0576d982afdb3c2bc8ca3372da1045
Alex Crichton <alex@alexcrichton.com>
2026-07-22 09:13:19 -0500

    Remove pthread_{kill,sigmask} stubs (#848)
    These are already not present in the header file (gated out). Take the
    same route as all other signal-related functions by removing them
    entirely instead of providing stubs.

--- libc-top-half/musl/src/thread/coop-threads/pthread_kill.c
+++
@@ -1,3 +0,0 @@
-#include "pthread_impl.h"
-
-int pthread_kill(pthread_t t, int sig) { return ENOTSUP; }
--- libc-top-half/musl/src/thread/coop-threads/pthread_sigmask.c
+++
@@ -1,7 +0,0 @@
-#include "pthread_impl.h"
-
-struct sigset_t;
-int pthread_sigmask(int how, const sigset_t *restrict set,
-                    sigset_t *restrict old) {
-  return ENOTSUP;
-}
